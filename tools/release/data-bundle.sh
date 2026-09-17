1lifedo1thing/qingjian

build(release): 产品数据改成不可变的 data-vN Release，仓库锁文件钉版本
滚动覆盖的 data Release 让老提交自编译拿到新数据、CI 只按 Release 自己的 SHA256SUMS 自校验。
现在 data-bundle.sh 发新号并写 tools/release/data.lock，data-fetch.sh 按锁文件下载校验，CI 与自编译同一条路；
build-info.json 多记 data_tag。老 data Release 冻结不删。

tools/release/data-bundle.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
-# 产品数据不在 git 里（data/ 整个 gitignore），CI 打包时从仓库的 `data` Release 下载。
-# 这个脚本把本机 data/generated/ 里输入法要随包的文件打成 qingjian-data.tar.gz，
-# 把本地整句模型 data/model/model.qjm（训练仓库 ../train 导出三件套，tools/release/pack-model.sh 打成一个文件）原样上传，
-# 把 LLM 生成的中间产物（续跑用的 JSONL）打成 qingjian-llm-intermediates.tar.gz，上传（覆盖）到 `data` Release。
+# 把本机 data/generated/ 与 data/model/model.qjm 发成一版不可变的数据 Release（data-vN，预发布），并写 tools/release/data.lock。
+# CI 与自编译按锁文件取数据（data-fetch.sh）；改了数据发新号，锁文件与用到新数据的代码同一个提交。
 #
-#   tools/release/data-bundle.sh           # 打包并上传
-#   tools/release/data-bundle.sh --pack    # 只打包到 target/release-data/，不上传
-#
-# `data` 是一个滚动的预发布（prerelease）Release：预发布不会成为 GitHub 的 latest，
-# 所以官网取 releases/latest/download/releases.json 时拿到的仍是最新的版本发布。
-# 词库 / 语言模型 / 释义表重生成或模型重训之后重跑一次即可；每次覆盖，不留历史。
+#   tools/release/data-bundle.sh                 # 发到下一个 data-vN
+#   tools/release/data-bundle.sh --tag data-v7   # 指定标签；已存在就拒绝
+#   tools/release/data-bundle.sh --pack          # 只打包到 target/release-data/
 set -euo pipefail
 
 ROOT="$(cd "$(dirname "$0")/../.." && pwd)"
 OUT="$ROOT/target/release-data"
+LOCK="$ROOT/tools/release/data.lock"
 cd "$ROOT"
+
+MODE=upload
+TAG=""
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --pack) MODE=pack; shift ;;
+    --tag) TAG="$2"; shift 2 ;;
+    *) echo "未知参数 $1" >&2; exit 1 ;;
+  esac
+done
 
 PRODUCT_FILES=(dict.qj lm.qj glossary-en.qj glossary-ja.qj glossary-zh.qj glossary-es.qj english.tsv english-frequency.tsv)
 MODEL_FILE=data/model/model.qjm
@@ -25,33 +31,38 @@ for f in "${PRODUCT_FILES[@]}"; do
 done
 DOMAIN_FILES=()
 for f in data/generated/dicts/*.qj; do [[ -f "$f" ]] && DOMAIN_FILES+=("dicts/$(basename "$f")"); done
-[[ ${#DOMAIN_FILES[@]} -gt 0 ]] || { echo "缺少 data/generated/dicts/*.qj（领域词库）" >&2; exit 1; }
-# 三件套比 .qjm 新（重训了没重打）就重打；没有三件套也没有 .qjm 就停（没有模型的包不会重排）
-[[ -f data/model/model.safetensors || -f "$MODEL_FILE" ]] || { echo "缺少 $MODEL_FILE，先在训练仓库导出三件套到 data/model/ 再跑 tools/release/pack-model.sh" >&2; exit 1; }
+[[ ${#DOMAIN_FILES[@]} -gt 0 ]] || { echo "缺少 data/generated/dicts/*.qj" >&2; exit 1; }
+[[ -f data/model/model.safetensors || -f "$MODEL_FILE" ]] || { echo "缺少 $MODEL_FILE（训练仓库导出三件套到 data/model/ 再跑 tools/release/pack-model.sh）" >&2; exit 1; }
+# 三件套比 .qjm 新就重打
 [[ -f data/model/model.safetensors ]] && tools/release/pack-model.sh
 
 rm -rf "$OUT" && mkdir -p "$OUT"
-# 路径相对 data/generated/，CI 解到 data/generated/ 就与本机一样
 tar -czf "$OUT/qingjian-data.tar.gz" -C data/generated "${PRODUCT_FILES[@]}" "${DOMAIN_FILES[@]}"
-# 模型单独一个文件：只重训模型时不用重传词库；CI 放到 data/model/，bundle.sh / qingjian.iss 见到就随包（.qjm 内部已是 fp16，不再压）
 cp "$MODEL_FILE" "$OUT/model.qjm"
 present=()
 for f in "${LLM_FILES[@]}"; do [[ -f "data/generated/$f" ]] && present+=("$f"); done
-if [[ ${#present[@]} -gt 0 ]]; then
-  tar -czf "$OUT/qingjian-llm-intermediates.tar.gz" -C data/generated "${present[@]}"
-fi
+[[ ${#present[@]} -gt 0 ]] && tar -czf "$OUT/qingjian-llm-intermediates.tar.gz" -C data/generated "${present[@]}"
 (cd "$OUT" && shasum -a 256 ./*.tar.gz ./model.qjm | tee SHA256SUMS)
 du -h "$OUT"/*.tar.gz "$OUT/model.qjm"
 
-[[ "${1:-}" == "--pack" ]] && exit 0
+[[ "$MODE" == "pack" ]] && exit 0
 
-if ! gh release view data >/dev/null 2>&1; then
-  gh release create data --prerelease --title "产品数据（CI 打包用）" \
-    --notes "输入法随包的词库 / 语言模型 / 释义表（qingjian-data.tar.gz）、本地整句模型单文件（model.qjm）与 LLM 生成的续跑中间产物（qingjian-llm-intermediates.tar.gz）。滚动覆盖，不是软件版本。由 tools/release/data-bundle.sh 上传。"
-else
-  gh release edit data --notes "输入法随包的词库 / 语言模型 / 释义表（qingjian-data.tar.gz）、本地整句模型单文件（model.qjm）与 LLM 生成的续跑中间产物（qingjian-llm-intermediates.tar.gz）。滚动覆盖，不是软件版本。由 tools/release/data-bundle.sh 上传。"
+if [[ -z "$TAG" ]]; then
+  last="$(gh release list --limit 200 --json tagName --jq '.[].tagName' | grep -E '^data-v[0-9]+$' | sed 's/data-v//' | sort -n | tail -1)"
+  TAG="data-v$(( ${last:-0} + 1 ))"
 fi
-gh release upload data "$OUT"/*.tar.gz "$OUT/model.qjm" "$OUT/SHA256SUMS" --clobber
-# 早先上传的三件套 tar 已被 model.qjm 取代
-gh release delete-asset data qingjian-model.tar.gz --yes 2>/dev/null || true
-echo "已上传到 Release: data"
+[[ "$TAG" =~ ^data-v[0-9]+$ ]] || { echo "标签要写成 data-vN：$TAG" >&2; exit 1; }
+gh release view "$TAG" >/dev/null 2>&1 && { echo "$TAG 已存在，数据版本不覆盖" >&2; exit 1; }
+
+sha_of() { grep " ./$1\$" "$OUT/SHA256SUMS" | cut -d' ' -f1; }
+gh release create "$TAG" --prerelease --target "$(git rev-parse HEAD)" --title "产品数据 $TAG" \
+  --notes "词库 / 语言模型 / 释义表（qingjian-data.tar.gz）、本地整句模型（model.qjm）、LLM 续跑中间产物（qingjian-llm-intermediates.tar.gz）。不可变；仓库 tools/release/data.lock 钉住要用哪一版。" \
+  "$OUT"/*.tar.gz "$OUT/model.qjm" "$OUT/SHA256SUMS"
+
+cat > "$LOCK" <<EOF
+# 产品数据版本，data-bundle.sh 写、data-fetch.sh 读；不要手改
+tag = $TAG
+qingjian-data.tar.gz = $(sha_of qingjian-data.tar.gz)
+model.qjm = $(sha_of model.qjm)
+EOF
+echo "已发 $TAG，锁文件已更新（记得提交）"

tools/release/data-fetch.sh
@@ -1,0 +1,55 @@
+#!/usr/bin/env bash
+# 按 tools/release/data.lock 下载产品数据并校验：qingjian-data.tar.gz 解到 data/generated/，model.qjm 放到 data/model/。
+#
+#   tools/release/data-fetch.sh            # 下载 + 校验 + 解开
+#   tools/release/data-fetch.sh --verify   # 只校验 target/release-data/ 里已下载的文件
+set -euo pipefail
+
+ROOT="$(cd "$(dirname "$0")/../.." && pwd)"
+LOCK="$ROOT/tools/release/data.lock"
+OUT="$ROOT/target/release-data"
+REPO="qingjian-team/qingjian"
+ASSETS=(qingjian-data.tar.gz model.qjm)
+cd "$ROOT"
+
+[[ -f "$LOCK" ]] || { echo "缺少 $LOCK" >&2; exit 1; }
+lock_value() { grep -E "^$1 *= *" "$LOCK" | head -1 | sed -E 's/^[^=]*= *//' | tr -d '[:space:]'; }
+sha256() { if command -v sha256sum >/dev/null 2>&1; then sha256sum "$1"; else shasum -a 256 "$1"; fi | cut -d' ' -f1; }
+
+TAG="$(lock_value tag)"
+[[ -n "$TAG" ]] || { echo "$LOCK 里没有 tag" >&2; exit 1; }
+mkdir -p "$OUT"
+
+if [[ "${1:-}" != "--verify" ]]; then
+  for f in "${ASSETS[@]}"; do
+    rm -f "$OUT/$f"
+    if command -v gh >/dev/null 2>&1 && gh auth status >/dev/null 2>&1; then
+      gh release download "$TAG" --repo "$REPO" --pattern "$f" --dir "$OUT"
+    else
+      curl -fL --retry 3 -o "$OUT/$f" "https://github.com/$REPO/releases/download/$TAG/$f"
+    fi
+  done
+fi
+
+for f in "${ASSETS[@]}"; do
+  expected="$(lock_value "$f")"
+  actual="$(sha256 "$OUT/$f")"
+  [[ -n "$expected" && "$actual" == "$expected" ]] || { echo "$f 与 data.lock 不符（$TAG）：期望 $expected，实际 $actual" >&2; exit 1; }
+  echo "$f  $actual"
+done
+[[ "${1:-}" == "--verify" ]] && exit 0
+
+mkdir -p data/generated data/model
+tar -xzf "$OUT/qingjian-data.tar.gz" -C data/generated
+cp "$OUT/model.qjm" data/model/model.qjm
+# 解出来的 mtime 比 checkout 出来的 TSV 旧，bundle.sh 会以为要重打
+find data/generated data/model -type f -exec touch {} +
+echo "产品数据 $TAG 已就位"
+
+if [[ -n "${GITHUB_ENV:-}" ]]; then
+  {
+    echo "DATA_TAG=$TAG"
+    echo "DATA_SHA256=$(lock_value qingjian-data.tar.gz)"
+    echo "MODEL_SHA256=$(lock_value model.qjm)"
+  } >> "$GITHUB_ENV"
+fi

rambocode/glimmer

(cherry picked from commit 60156cc4e0ad3a819a143155879c9bd30ca6e321)

tools/release/data-bundle.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
-# 产品数据不在 git 里（data/ 整个 gitignore），CI 打包时从仓库的 `data` Release 下载。
-# 这个脚本把本机 data/generated/ 里输入法要随包的文件打成 glimmer-data.tar.gz，
-# 把本地整句模型 data/model/model.qjm（训练仓库 ../train 导出三件套，tools/release/pack-model.sh 打成一个文件）原样上传，
-# 把 LLM 生成的中间产物（续跑用的 JSONL）打成 glimmer-llm-intermediates.tar.gz，上传（覆盖）到 `data` Release。
+# 把本机 data/generated/ 与 data/model/model.qjm 发成一版不可变的数据 Release（data-vN，预发布），并写 tools/release/data.lock。
+# CI 与自编译按锁文件取数据（data-fetch.sh）；改了数据发新号，锁文件与用到新数据的代码同一个提交。
 #
-#   tools/release/data-bundle.sh           # 打包并上传
-#   tools/release/data-bundle.sh --pack    # 只打包到 target/release-data/，不上传
-#
-# `data` 是一个滚动的预发布（prerelease）Release：预发布不会成为 GitHub 的 latest，
-# 所以官网取 releases/latest/download/releases.json 时拿到的仍是最新的版本发布。
-# 词库 / 语言模型 / 释义表重生成或模型重训之后重跑一次即可；每次覆盖，不留历史。
+#   tools/release/data-bundle.sh                 # 发到下一个 data-vN
+#   tools/release/data-bundle.sh --tag data-v7   # 指定标签；已存在就拒绝
+#   tools/release/data-bundle.sh --pack          # 只打包到 target/release-data/
 set -euo pipefail
 
 ROOT="$(cd "$(dirname "$0")/../.." && pwd)"
 OUT="$ROOT/target/release-data"
+LOCK="$ROOT/tools/release/data.lock"
 cd "$ROOT"
+
+MODE=upload
+TAG=""
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --pack) MODE=pack; shift ;;
+    --tag) TAG="$2"; shift 2 ;;
+    *) echo "未知参数 $1" >&2; exit 1 ;;
+  esac
+done
 
 PRODUCT_FILES=(dict.qj lm.qj glossary-en.qj glossary-ja.qj glossary-zh.qj glossary-es.qj english.tsv english-frequency.tsv wubi86.qj wubi98.qj wubixsj.qj)
 MODEL_FILE=data/model/model.qjm
@@ -25,33 +31,38 @@ for f in "${PRODUCT_FILES[@]}"; do
 done
 DOMAIN_FILES=()
 for f in data/generated/dicts/*.qj; do [[ -f "$f" ]] && DOMAIN_FILES+=("dicts/$(basename "$f")"); done
-[[ ${#DOMAIN_FILES[@]} -gt 0 ]] || { echo "缺少 data/generated/dicts/*.qj（领域词库）" >&2; exit 1; }
-# 三件套比 .qjm 新（重训了没重打）就重打；没有三件套也没有 .qjm 就停（没有模型的包不会重排）
-[[ -f data/model/model.safetensors || -f "$MODEL_FILE" ]] || { echo "缺少 $MODEL_FILE，先在训练仓库导出三件套到 data/model/ 再跑 tools/release/pack-model.sh" >&2; exit 1; }
+[[ ${#DOMAIN_FILES[@]} -gt 0 ]] || { echo "缺少 data/generated/dicts/*.qj" >&2; exit 1; }
+[[ -f data/model/model.safetensors || -f "$MODEL_FILE" ]] || { echo "缺少 $MODEL_FILE（训练仓库导出三件套到 data/model/ 再跑 tools/release/pack-model.sh）" >&2; exit 1; }
+# 三件套比 .qjm 新就重打
 [[ -f data/model/model.safetensors ]] && tools/release/pack-model.sh
 
 rm -rf "$OUT" && mkdir -p "$OUT"
-# 路径相对 data/generated/，CI 解到 data/generated/ 就与本机一样
 tar -czf "$OUT/glimmer-data.tar.gz" -C data/generated "${PRODUCT_FILES[@]}" "${DOMAIN_FILES[@]}"
-# 模型单独一个文件：只重训模型时不用重传词库；CI 放到 data/model/，bundle.sh / glimmer.iss 见到就随包（.qjm 内部已是 fp16，不再压）
 cp "$MODEL_FILE" "$OUT/model.qjm"
 present=()
 for f in "${LLM_FILES[@]}"; do [[ -f "data/generated/$f" ]] && present+=("$f"); done
-if [[ ${#present[@]} -gt 0 ]]; then
-  tar -czf "$OUT/glimmer-llm-intermediates.tar.gz" -C data/generated "${present[@]}"
-fi
+[[ ${#present[@]} -gt 0 ]] && tar -czf "$OUT/glimmer-llm-intermediates.tar.gz" -C data/generated "${present[@]}"
 (cd "$OUT" && shasum -a 256 ./*.tar.gz ./model.qjm | tee SHA256SUMS)
 du -h "$OUT"/*.tar.gz "$OUT/model.qjm"
 
-[[ "${1:-}" == "--pack" ]] && exit 0
+[[ "$MODE" == "pack" ]] && exit 0
 
-if ! gh release view data >/dev/null 2>&1; then
-  gh release create data --prerelease --title "产品数据（CI 打包用）" \
-    --notes "输入法随包的词库 / 语言模型 / 释义表（glimmer-data.tar.gz）、本地整句模型单文件（model.qjm）与 LLM 生成的续跑中间产物（glimmer-llm-intermediates.tar.gz）。滚动覆盖，不是软件版本。由 tools/release/data-bundle.sh 上传。"
-else
-  gh release edit data --notes "输入法随包的词库 / 语言模型 / 释义表（glimmer-data.tar.gz）、本地整句模型单文件（model.qjm）与 LLM 生成的续跑中间产物（glimmer-llm-intermediates.tar.gz）。滚动覆盖，不是软件版本。由 tools/release/data-bundle.sh 上传。"
+if [[ -z "$TAG" ]]; then
+  last="$(gh release list --limit 200 --json tagName --jq '.[].tagName' | grep -E '^data-v[0-9]+$' | sed 's/data-v//' | sort -n | tail -1)"
+  TAG="data-v$(( ${last:-0} + 1 ))"
 fi
-gh release upload data "$OUT"/*.tar.gz "$OUT/model.qjm" "$OUT/SHA256SUMS" --clobber
-# 早先上传的三件套 tar 已被 model.qjm 取代
-gh release delete-asset data glimmer-model.tar.gz --yes 2>/dev/null || true
-echo "已上传到 Release: data"
+[[ "$TAG" =~ ^data-v[0-9]+$ ]] || { echo "标签要写成 data-vN：$TAG" >&2; exit 1; }
+gh release view "$TAG" >/dev/null 2>&1 && { echo "$TAG 已存在，数据版本不覆盖" >&2; exit 1; }
+
+sha_of() { grep " ./$1\$" "$OUT/SHA256SUMS" | cut -d' ' -f1; }
+gh release create "$TAG" --prerelease --target "$(git rev-parse HEAD)" --title "产品数据 $TAG" \
+  --notes "词库 / 语言模型 / 释义表（glimmer-data.tar.gz）、本地整句模型（model.qjm）、LLM 续跑中间产物（glimmer-llm-intermediates.tar.gz）。不可变；仓库 tools/release/data.lock 钉住要用哪一版。" \
+  "$OUT"/*.tar.gz "$OUT/model.qjm" "$OUT/SHA256SUMS"
+
+cat > "$LOCK" <<EOF
+# 产品数据版本，data-bundle.sh 写、data-fetch.sh 读；不要手改
+tag = $TAG
+glimmer-data.tar.gz = $(sha_of glimmer-data.tar.gz)
+model.qjm = $(sha_of model.qjm)
+EOF
+echo "已发 $TAG，锁文件已更新（记得提交）"

tools/release/data-fetch.sh
@@ -1,0 +1,55 @@
+#!/usr/bin/env bash
+# 按 tools/release/data.lock 下载产品数据并校验：glimmer-data.tar.gz 解到 data/generated/，model.qjm 放到 data/model/。
+#
+#   tools/release/data-fetch.sh            # 下载 + 校验 + 解开
+#   tools/release/data-fetch.sh --verify   # 只校验 target/release-data/ 里已下载的文件
+set -euo pipefail
+
+ROOT="$(cd "$(dirname "$0")/../.." && pwd)"
+LOCK="$ROOT/tools/release/data.lock"
+OUT="$ROOT/target/release-data"
+REPO="rambocode/glimmer"
+ASSETS=(glimmer-data.tar.gz model.qjm)
+cd "$ROOT"
+
+[[ -f "$LOCK" ]] || { echo "缺少 $LOCK" >&2; exit 1; }
+lock_value() { grep -E "^$1 *= *" "$LOCK" | head -1 | sed -E 's/^[^=]*= *//' | tr -d '[:space:]'; }
+sha256() { if command -v sha256sum >/dev/null 2>&1; then sha256sum "$1"; else shasum -a 256 "$1"; fi | cut -d' ' -f1; }
+
+TAG="$(lock_value tag)"
+[[ -n "$TAG" ]] || { echo "$LOCK 里没有 tag" >&2; exit 1; }
+mkdir -p "$OUT"
+
+if [[ "${1:-}" != "--verify" ]]; then
+  for f in "${ASSETS[@]}"; do
+    rm -f "$OUT/$f"
+    if command -v gh >/dev/null 2>&1 && gh auth status >/dev/null 2>&1; then
+      gh release download "$TAG" --repo "$REPO" --pattern "$f" --dir "$OUT"
+    else
+      curl -fL --retry 3 -o "$OUT/$f" "https://github.com/$REPO/releases/download/$TAG/$f"
+    fi
+  done
+fi
+
+for f in "${ASSETS[@]}"; do
+  expected="$(lock_value "$f")"
+  actual="$(sha256 "$OUT/$f")"
+  [[ -n "$expected" && "$actual" == "$expected" ]] || { echo "$f 与 data.lock 不符（$TAG）：期望 $expected，实际 $actual" >&2; exit 1; }
+  echo "$f  $actual"
+done
+[[ "${1:-}" == "--verify" ]] && exit 0
+
+mkdir -p data/generated data/model
+tar -xzf "$OUT/glimmer-data.tar.gz" -C data/generated
+cp "$OUT/model.qjm" data/model/model.qjm
+# 解出来的 mtime 比 checkout 出来的 TSV 旧，bundle.sh 会以为要重打
+find data/generated data/model -type f -exec touch {} +
+echo "产品数据 $TAG 已就位"
+
+if [[ -n "${GITHUB_ENV:-}" ]]; then
+  {
+    echo "DATA_TAG=$TAG"
+    echo "DATA_SHA256=$(lock_value glimmer-data.tar.gz)"
+    echo "MODEL_SHA256=$(lock_value model.qjm)"
+  } >> "$GITHUB_ENV"
+fi