1lifedo1thing/qingjian

Core：统一 lue/nue 与 lve/nve 的韵母写法（#60）
Closes #59

crates/qingjian-core/src/fuzzy/rules.rs
@@ -1,4 +1,4 @@
-use qingjian_dictionary::SyllablePattern;
+use qingjian_dictionary::{SyllablePattern, canonical_syllable};
 use serde::{Deserialize, Serialize};
 
 use super::Expanded;
@@ -117,11 +117,11 @@ impl FuzzyRules {
 
     /// `syllable` 是不是 `typed` 按当前规则的一种模糊写法（不含它自己）。
     pub fn is_variant(&self, typed: &str, syllable: &str) -> bool {
-        typed != syllable
+        canonical_syllable(typed) != canonical_syllable(syllable)
             && self
                 .alternatives(SyllablePattern::complete(typed))
                 .iter()
-                .any(|form| form == syllable)
+                .any(|form| canonical_syllable(form) == canonical_syllable(syllable))
     }
 
     /// 一个音节的全部写法，第一个是敲的原文；没开模糊音就只有它自己。

crates/qingjian-core/src/parser/segmentation.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 
-use qingjian_dictionary::SyllablePattern;
+use qingjian_dictionary::{SyllablePattern, canonical_syllable};
 
 /// 切分出的一个音节。
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -29,7 +29,7 @@ impl Syllable {
 
     pub fn pattern(&self) -> SyllablePattern<'_> {
         SyllablePattern {
-            text: &self.text,
+            text: canonical_syllable(&self.text),
             complete: self.complete,
         }
     }

crates/qingjian-core/src/sentence/span/cache.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::sync::Arc;
 
-use qingjian_dictionary::SyllablePattern;
+use qingjian_dictionary::{SyllablePattern, canonical_syllable};
 
 use super::word::SpanWord;
 
@@ -31,7 +31,7 @@ impl SpanCache {
                 if alt > 0 {
                     key.push('|');
                 }
-                key.push_str(pattern.text);
+                key.push_str(canonical_syllable(pattern.text));
                 if !pattern.complete {
                     key.push('…');
                 }

crates/qingjian-dictionary/src/dictionary.rs
@@ -6,7 +6,7 @@ use zerocopy::{FromBytes, Immutable, IntoBytes, KnownLayout};
 
 use crate::error::DictionaryError;
 use crate::matching::Match;
-use crate::pattern::SyllablePattern;
+use crate::pattern::{SyllablePattern, canonical_syllable};
 
 /// 收窄到多小的区间就改成逐键比对。
 const LINEAR_SCAN_LIMIT: usize = 48;
@@ -144,7 +144,7 @@ impl Dictionary {
                 if !first {
                     keys.push(' ');
                 }
-                keys.push_str(syllable);
+                keys.push_str(canonical_syllable(syllable));
                 first = false;
             }
             let actual = keys.len() as u32 - rows.last().unwrap().key_start;
@@ -240,6 +240,45 @@ impl Dictionary {
                     "key points outside the slot table",
                 ));
             }
+        }
+        let has_legacy_keys = index.iter().any(|entry| {
+            let start = entry.key_start as usize;
+            keys[start..start + usize::from(entry.key_len)]
+                .split(' ')
+                .any(|syllable| canonical_syllable(syllable) != syllable)
+        });
+        if has_legacy_keys {
+            let metadata = container.metadata().clone();
+            let mut owned_keys = String::with_capacity(keys.len());
+            let mut rows = Vec::with_capacity(slots.len());
+            for entry in index.iter() {
+                let start = entry.key_start as usize;
+                let key = &keys[start..start + usize::from(entry.key_len)];
+                let key_start = owned_keys.len() as u32;
+                for (position, syllable) in key.split(' ').enumerate() {
+                    if position > 0 {
+                        owned_keys.push(' ');
+                    }
+                    owned_keys.push_str(canonical_syllable(syllable));
+                }
+                let key_len = (owned_keys.len() as u32 - key_start) as u16;
+                let first = entry.first_slot as usize;
+                for slot in &slots[first..first + entry.slot_count as usize] {
+                    rows.push(Row {
+                        key_start,
+                        key_len,
+                        slot: *slot,
+                    });
+                }
+            }
+            let mut dictionary = Self::assemble(texts.to_string(), owned_keys, rows);
+            dictionary.metadata = Some(metadata);
+            tracing::debug!(
+                entries = dictionary.slots.len(),
+                keys = dictionary.index.len(),
+                "旧词库键已规范化"
+            );
+            return Ok(dictionary);
         }
         tracing::debug!(
             entries = slots.len(),
@@ -397,7 +436,7 @@ impl Dictionary {
         let base_len = prefix.len();
         for current in positions[depth].as_ref() {
             prefix.truncate(base_len);
-            prefix.push_str(current.text);
+            prefix.push_str(canonical_syllable(current.text));
             let sub = self.prefix_range(prefix, range.clone());
             if sub.is_empty() {
                 continue;
@@ -642,6 +681,16 @@ mod tests {
     }
 
     #[test]
+    fn normalizes_u_umlaut_keys_and_queries() {
+        let dictionary = Dictionary::parse("略\tlue\t100\n虐\tnve\t90\n").unwrap();
+        assert_eq!(dictionary.entries().next().unwrap().pinyin, "lve");
+        assert_eq!(texts(&dictionary.lookup(&["lue"], false)), ["略"]);
+        assert_eq!(texts(&dictionary.lookup(&["lve"], false)), ["略"]);
+        assert_eq!(texts(&dictionary.lookup(&["nue"], false)), ["虐"]);
+        assert_eq!(texts(&dictionary.lookup(&["nve"], false)), ["虐"]);
+    }
+
+    #[test]
     fn rejects_malformed_line() {
         let error = Dictionary::parse("开发\tkai fa\tabc\n").unwrap_err();
         assert!(matches!(error, DictionaryError::Line { line: 1, .. }));
@@ -740,6 +789,10 @@ mod tests {
         dictionary.write_qj(&path, &metadata).unwrap();
         let mapped = Dictionary::from_path(&path).unwrap();
         std::fs::remove_file(&path).unwrap();
+        assert!(matches!(&mapped.texts, Text::Mapped { .. }));
+        assert!(matches!(&mapped.keys, Text::Mapped { .. }));
+        assert!(matches!(&mapped.index, Table::Mapped { .. }));
+        assert!(matches!(&mapped.slots, Table::Mapped { .. }));
         assert_eq!(mapped.len(), dictionary.len());
         assert_eq!(mapped.total_frequency(), dictionary.total_frequency());
         assert_eq!(mapped.metadata().unwrap().name, "测试词库");
@@ -754,5 +807,66 @@ mod tests {
             let reopened = mapped.lookup(&syllables, partial);
             assert_eq!(texts(&reopened), texts(&original), "{syllables:?}");
         }
+    }
+
+    #[test]
+    fn legacy_qj_umlaut_keys_remain_queryable() {
+        let legacy = Dictionary {
+            texts: Text::Owned("略虐".to_owned()),
+            keys: Text::Owned("luenue".to_owned()),
+            index: Table::Owned(vec![
+                KeyIndex {
+                    key_start: 0,
+                    first_slot: 0,
+                    slot_count: 1,
+                    key_len: 3,
+                    reserved: 0,
+                },
+                KeyIndex {
+                    key_start: 3,
+                    first_slot: 1,
+                    slot_count: 1,
+                    key_len: 3,
+                    reserved: 0,
+                },
+            ]),
+            slots: Table::Owned(vec![
+                Slot {
+                    text_start: 0,
+                    frequency: 100,
+                    text_len: 3,
+                    reserved: 0,
+                },
+                Slot {
+                    text_start: 3,
+                    frequency: 90,
+                    text_len: 3,
+                    reserved: 0,
+                },
+            ]),
+            total_frequency: 190,
+            metadata: None,
+        };
+        let dir = std::env::temp_dir().join("qingjian-dictionary-tests");
+        std::fs::create_dir_all(&dir).unwrap();
+        let path = dir.join(format!("legacy-dict-{}.qj", std::process::id()));
+        let metadata = Metadata {
+            name: "旧词库".to_owned(),
+            ..Metadata::default()
+        };
+        legacy.write_qj(&path, &metadata).unwrap();
+
+        let dictionary = Dictionary::open_qj(&path).unwrap();
+        std::fs::remove_file(&path).unwrap();
+
+        assert!(matches!(&dictionary.texts, Text::Owned(_)));
+        assert!(matches!(&dictionary.keys, Text::Owned(_)));
+        assert!(matches!(&dictionary.index, Table::Owned(_)));
+        assert!(matches!(&dictionary.slots, Table::Owned(_)));
+        assert_eq!(dictionary.metadata().unwrap().name, "旧词库");
+        assert_eq!(texts(&dictionary.lookup(&["lue"], false)), ["略"]);
+        assert_eq!(texts(&dictionary.lookup(&["lve"], false)), ["略"]);
+        assert_eq!(texts(&dictionary.lookup(&["nue"], false)), ["虐"]);
+        assert_eq!(texts(&dictionary.lookup(&["nve"], false)), ["虐"]);
     }
 }

tools/dict-convert/src/lexicon/annotations.rs
@@ -3,6 +3,7 @@
 use std::collections::HashMap;
 use std::path::Path;
 
+use qingjian_dictionary::canonical_syllable;
 use serde::Deserialize;
 
 use crate::error::ConvertError;
@@ -23,11 +24,36 @@ pub fn load(path: &Path) -> Result<HashMap<String, Vec<String>>, ConvertError> {
             continue;
         }
         match serde_json::from_str::<Row>(line) {
-            Ok(row) => {
+            Ok(mut row) => {
+                for syllable in &mut row.pinyin {
+                    *syllable = canonical_syllable(syllable).to_owned();
+                }
                 out.insert(row.word, row.pinyin);
             }
             Err(error) => tracing::warn!(%error, "拼音标注坏行，跳过"),
         }
     }
     Ok(out)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn normalizes_u_umlaut_spellings() {
+        let path = std::env::temp_dir().join(format!(
+            "qingjian-annotations-normalize-{}.jsonl",
+            std::process::id()
+        ));
+        std::fs::write(
+            &path,
+            "{\"word\":\"策略\",\"pinyin\":[\"ce\",\"lue\"]}\n{\"word\":\"虐待\",\"pinyin\":[\"nue\",\"dai\"]}\n",
+        )
+        .unwrap();
+        let annotations = load(&path).unwrap();
+        std::fs::remove_file(path).unwrap();
+        assert_eq!(annotations["策略"], ["ce", "lve"]);
+        assert_eq!(annotations["虐待"], ["nve", "dai"]);
+    }
 }

tools/dict-convert/src/lexicon/corpus.rs
@@ -3,6 +3,8 @@
 use std::collections::HashMap;
 use std::path::Path;
 
+use qingjian_dictionary::canonical_syllable;
+
 use crate::error::ConvertError;
 
 pub fn load(path: &Path) -> Result<HashMap<String, u64>, ConvertError> {
@@ -46,7 +48,11 @@ pub fn extra_words(path: &Path) -> Result<Vec<ExtraWord>, ConvertError> {
             let count = fields.next()?.trim().parse().ok()?;
             let syllables = fields
                 .next()
-                .map(|p| p.split_whitespace().map(str::to_owned).collect::<Vec<_>>())
+                .map(|p| {
+                    p.split_whitespace()
+                        .map(|s| canonical_syllable(s).to_owned())
+                        .collect::<Vec<_>>()
+                })
                 .filter(|s| !s.is_empty());
             Some(ExtraWord {
                 text,
@@ -55,4 +61,22 @@ pub fn extra_words(path: &Path) -> Result<Vec<ExtraWord>, ConvertError> {
             })
         })
         .collect())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn normalizes_given_pinyin() {
+        let path = std::env::temp_dir().join(format!(
+            "qingjian-extra-words-normalize-{}.tsv",
+            std::process::id()
+        ));
+        std::fs::write(&path, "策略\t20\tce lue\n虐待\t10\tnue dai\n").unwrap();
+        let words = extra_words(&path).unwrap();
+        std::fs::remove_file(path).unwrap();
+        assert_eq!(words[0].syllables.as_deref().unwrap(), ["ce", "lve"]);
+        assert_eq!(words[1].syllables.as_deref().unwrap(), ["nve", "dai"]);
+    }
 }

tools/dict-convert/src/lexicon/tone.rs
@@ -1,6 +1,8 @@
 //! 拼音写法归一：带声调符号的（Unihan 的 `xíng`）与带声调数字的（通用词表的 `wei4'shen2'me`）都转成
 //! 不带声调、ü 写 v 的小写音节。
 
+use qingjian_dictionary::canonical_syllable;
+
 /// `xíng` → `xing`，`lǜ` → `lv`，`ê̄` 这类罕见记号转成对应字母；组合用变音符号（U+0300–U+036F）丢掉。
 pub fn strip_tone(reading: &str) -> String {
     let mut out = String::with_capacity(reading.len());
@@ -20,7 +22,7 @@ pub fn strip_tone(reading: &str) -> String {
         };
         out.push(plain);
     }
-    out
+    canonical_syllable(&out).to_owned()
 }
 
 /// 通用词表的 `wei4'shen2'me` → `["wei", "shen", "me"]`；`lv4` 保持 v。空段跳过。
@@ -42,6 +44,10 @@ mod tests {
         assert_eq!(strip_tone("xíng"), "xing");
         assert_eq!(strip_tone("lǜ"), "lv");
         assert_eq!(strip_tone("nǚ"), "nv");
+        assert_eq!(strip_tone("lüè"), "lve");
+        assert_eq!(strip_tone("nüè"), "nve");
+        assert_eq!(strip_tone("lue4"), "lve");
+        assert_eq!(strip_tone("nue4"), "nve");
         assert_eq!(strip_tone("shi4"), "shi");
         assert_eq!(strip_tone("de"), "de");
     }

rambocode/glimmer

(cherry picked from commit 8777e0b212865d8ab252547cda004c3b52b1281b)

crates/glimmer-core/src/fuzzy/rules.rs
@@ -1,4 +1,4 @@
-use glimmer_dictionary::SyllablePattern;
+use glimmer_dictionary::{SyllablePattern, canonical_syllable};
 use serde::{Deserialize, Serialize};
 
 use super::Expanded;
@@ -117,11 +117,11 @@ impl FuzzyRules {
 
     /// `syllable` 是不是 `typed` 按当前规则的一种模糊写法（不含它自己）。
     pub fn is_variant(&self, typed: &str, syllable: &str) -> bool {
-        typed != syllable
+        canonical_syllable(typed) != canonical_syllable(syllable)
             && self
                 .alternatives(SyllablePattern::complete(typed))
                 .iter()
-                .any(|form| form == syllable)
+                .any(|form| canonical_syllable(form) == canonical_syllable(syllable))
     }
 
     /// 一个音节的全部写法，第一个是敲的原文；没开模糊音就只有它自己。

crates/glimmer-core/src/parser/segmentation.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 
-use glimmer_dictionary::SyllablePattern;
+use glimmer_dictionary::{SyllablePattern, canonical_syllable};
 
 /// 切分出的一个音节。
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -29,7 +29,7 @@ impl Syllable {
 
     pub fn pattern(&self) -> SyllablePattern<'_> {
         SyllablePattern {
-            text: &self.text,
+            text: canonical_syllable(&self.text),
             complete: self.complete,
         }
     }

crates/glimmer-core/src/sentence/span/cache.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::sync::Arc;
 
-use glimmer_dictionary::SyllablePattern;
+use glimmer_dictionary::{SyllablePattern, canonical_syllable};
 
 use super::word::SpanWord;
 
@@ -31,7 +31,7 @@ impl SpanCache {
                 if alt > 0 {
                     key.push('|');
                 }
-                key.push_str(pattern.text);
+                key.push_str(canonical_syllable(pattern.text));
                 if !pattern.complete {
                     key.push('…');
                 }

crates/glimmer-dictionary/src/dictionary.rs
@@ -6,7 +6,7 @@ use zerocopy::{FromBytes, Immutable, IntoBytes, KnownLayout};
 
 use crate::error::DictionaryError;
 use crate::matching::Match;
-use crate::pattern::SyllablePattern;
+use crate::pattern::{SyllablePattern, canonical_syllable};
 
 /// 收窄到多小的区间就改成逐键比对。
 const LINEAR_SCAN_LIMIT: usize = 48;
@@ -144,7 +144,7 @@ impl Dictionary {
                 if !first {
                     keys.push(' ');
                 }
-                keys.push_str(syllable);
+                keys.push_str(canonical_syllable(syllable));
                 first = false;
             }
             let actual = keys.len() as u32 - rows.last().unwrap().key_start;
@@ -240,6 +240,45 @@ impl Dictionary {
                     "key points outside the slot table",
                 ));
             }
+        }
+        let has_legacy_keys = index.iter().any(|entry| {
+            let start = entry.key_start as usize;
+            keys[start..start + usize::from(entry.key_len)]
+                .split(' ')
+                .any(|syllable| canonical_syllable(syllable) != syllable)
+        });
+        if has_legacy_keys {
+            let metadata = container.metadata().clone();
+            let mut owned_keys = String::with_capacity(keys.len());
+            let mut rows = Vec::with_capacity(slots.len());
+            for entry in index.iter() {
+                let start = entry.key_start as usize;
+                let key = &keys[start..start + usize::from(entry.key_len)];
+                let key_start = owned_keys.len() as u32;
+                for (position, syllable) in key.split(' ').enumerate() {
+                    if position > 0 {
+                        owned_keys.push(' ');
+                    }
+                    owned_keys.push_str(canonical_syllable(syllable));
+                }
+                let key_len = (owned_keys.len() as u32 - key_start) as u16;
+                let first = entry.first_slot as usize;
+                for slot in &slots[first..first + entry.slot_count as usize] {
+                    rows.push(Row {
+                        key_start,
+                        key_len,
+                        slot: *slot,
+                    });
+                }
+            }
+            let mut dictionary = Self::assemble(texts.to_string(), owned_keys, rows);
+            dictionary.metadata = Some(metadata);
+            tracing::debug!(
+                entries = dictionary.slots.len(),
+                keys = dictionary.index.len(),
+                "旧词库键已规范化"
+            );
+            return Ok(dictionary);
         }
         tracing::debug!(
             entries = slots.len(),
@@ -397,7 +436,7 @@ impl Dictionary {
         let base_len = prefix.len();
         for current in positions[depth].as_ref() {
             prefix.truncate(base_len);
-            prefix.push_str(current.text);
+            prefix.push_str(canonical_syllable(current.text));
             let sub = self.prefix_range(prefix, range.clone());
             if sub.is_empty() {
                 continue;
@@ -642,6 +681,16 @@ mod tests {
     }
 
     #[test]
+    fn normalizes_u_umlaut_keys_and_queries() {
+        let dictionary = Dictionary::parse("略\tlue\t100\n虐\tnve\t90\n").unwrap();
+        assert_eq!(dictionary.entries().next().unwrap().pinyin, "lve");
+        assert_eq!(texts(&dictionary.lookup(&["lue"], false)), ["略"]);
+        assert_eq!(texts(&dictionary.lookup(&["lve"], false)), ["略"]);
+        assert_eq!(texts(&dictionary.lookup(&["nue"], false)), ["虐"]);
+        assert_eq!(texts(&dictionary.lookup(&["nve"], false)), ["虐"]);
+    }
+
+    #[test]
     fn rejects_malformed_line() {
         let error = Dictionary::parse("开发\tkai fa\tabc\n").unwrap_err();
         assert!(matches!(error, DictionaryError::Line { line: 1, .. }));
@@ -740,6 +789,10 @@ mod tests {
         dictionary.write_qj(&path, &metadata).unwrap();
         let mapped = Dictionary::from_path(&path).unwrap();
         std::fs::remove_file(&path).unwrap();
+        assert!(matches!(&mapped.texts, Text::Mapped { .. }));
+        assert!(matches!(&mapped.keys, Text::Mapped { .. }));
+        assert!(matches!(&mapped.index, Table::Mapped { .. }));
+        assert!(matches!(&mapped.slots, Table::Mapped { .. }));
         assert_eq!(mapped.len(), dictionary.len());
         assert_eq!(mapped.total_frequency(), dictionary.total_frequency());
         assert_eq!(mapped.metadata().unwrap().name, "测试词库");
@@ -754,5 +807,66 @@ mod tests {
             let reopened = mapped.lookup(&syllables, partial);
             assert_eq!(texts(&reopened), texts(&original), "{syllables:?}");
         }
+    }
+
+    #[test]
+    fn legacy_qj_umlaut_keys_remain_queryable() {
+        let legacy = Dictionary {
+            texts: Text::Owned("略虐".to_owned()),
+            keys: Text::Owned("luenue".to_owned()),
+            index: Table::Owned(vec![
+                KeyIndex {
+                    key_start: 0,
+                    first_slot: 0,
+                    slot_count: 1,
+                    key_len: 3,
+                    reserved: 0,
+                },
+                KeyIndex {
+                    key_start: 3,
+                    first_slot: 1,
+                    slot_count: 1,
+                    key_len: 3,
+                    reserved: 0,
+                },
+            ]),
+            slots: Table::Owned(vec![
+                Slot {
+                    text_start: 0,
+                    frequency: 100,
+                    text_len: 3,
+                    reserved: 0,
+                },
+                Slot {
+                    text_start: 3,
+                    frequency: 90,
+                    text_len: 3,
+                    reserved: 0,
+                },
+            ]),
+            total_frequency: 190,
+            metadata: None,
+        };
+        let dir = std::env::temp_dir().join("glimmer-dictionary-tests");
+        std::fs::create_dir_all(&dir).unwrap();
+        let path = dir.join(format!("legacy-dict-{}.qj", std::process::id()));
+        let metadata = Metadata {
+            name: "旧词库".to_owned(),
+            ..Metadata::default()
+        };
+        legacy.write_qj(&path, &metadata).unwrap();
+
+        let dictionary = Dictionary::open_qj(&path).unwrap();
+        std::fs::remove_file(&path).unwrap();
+
+        assert!(matches!(&dictionary.texts, Text::Owned(_)));
+        assert!(matches!(&dictionary.keys, Text::Owned(_)));
+        assert!(matches!(&dictionary.index, Table::Owned(_)));
+        assert!(matches!(&dictionary.slots, Table::Owned(_)));
+        assert_eq!(dictionary.metadata().unwrap().name, "旧词库");
+        assert_eq!(texts(&dictionary.lookup(&["lue"], false)), ["略"]);
+        assert_eq!(texts(&dictionary.lookup(&["lve"], false)), ["略"]);
+        assert_eq!(texts(&dictionary.lookup(&["nue"], false)), ["虐"]);
+        assert_eq!(texts(&dictionary.lookup(&["nve"], false)), ["虐"]);
     }
 }

tools/dict-convert/src/lexicon/annotations.rs
@@ -3,6 +3,7 @@
 use std::collections::HashMap;
 use std::path::Path;
 
+use glimmer_dictionary::canonical_syllable;
 use serde::Deserialize;
 
 use crate::error::ConvertError;
@@ -23,11 +24,36 @@ pub fn load(path: &Path) -> Result<HashMap<String, Vec<String>>, ConvertError> {
             continue;
         }
         match serde_json::from_str::<Row>(line) {
-            Ok(row) => {
+            Ok(mut row) => {
+                for syllable in &mut row.pinyin {
+                    *syllable = canonical_syllable(syllable).to_owned();
+                }
                 out.insert(row.word, row.pinyin);
             }
             Err(error) => tracing::warn!(%error, "拼音标注坏行，跳过"),
         }
     }
     Ok(out)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn normalizes_u_umlaut_spellings() {
+        let path = std::env::temp_dir().join(format!(
+            "glimmer-annotations-normalize-{}.jsonl",
+            std::process::id()
+        ));
+        std::fs::write(
+            &path,
+            "{\"word\":\"策略\",\"pinyin\":[\"ce\",\"lue\"]}\n{\"word\":\"虐待\",\"pinyin\":[\"nue\",\"dai\"]}\n",
+        )
+        .unwrap();
+        let annotations = load(&path).unwrap();
+        std::fs::remove_file(path).unwrap();
+        assert_eq!(annotations["策略"], ["ce", "lve"]);
+        assert_eq!(annotations["虐待"], ["nve", "dai"]);
+    }
 }

tools/dict-convert/src/lexicon/corpus.rs
@@ -3,6 +3,8 @@
 use std::collections::HashMap;
 use std::path::Path;
 
+use glimmer_dictionary::canonical_syllable;
+
 use crate::error::ConvertError;
 
 pub fn load(path: &Path) -> Result<HashMap<String, u64>, ConvertError> {
@@ -46,7 +48,11 @@ pub fn extra_words(path: &Path) -> Result<Vec<ExtraWord>, ConvertError> {
             let count = fields.next()?.trim().parse().ok()?;
             let syllables = fields
                 .next()
-                .map(|p| p.split_whitespace().map(str::to_owned).collect::<Vec<_>>())
+                .map(|p| {
+                    p.split_whitespace()
+                        .map(|s| canonical_syllable(s).to_owned())
+                        .collect::<Vec<_>>()
+                })
                 .filter(|s| !s.is_empty());
             Some(ExtraWord {
                 text,
@@ -55,4 +61,22 @@ pub fn extra_words(path: &Path) -> Result<Vec<ExtraWord>, ConvertError> {
             })
         })
         .collect())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn normalizes_given_pinyin() {
+        let path = std::env::temp_dir().join(format!(
+            "glimmer-extra-words-normalize-{}.tsv",
+            std::process::id()
+        ));
+        std::fs::write(&path, "策略\t20\tce lue\n虐待\t10\tnue dai\n").unwrap();
+        let words = extra_words(&path).unwrap();
+        std::fs::remove_file(path).unwrap();
+        assert_eq!(words[0].syllables.as_deref().unwrap(), ["ce", "lve"]);
+        assert_eq!(words[1].syllables.as_deref().unwrap(), ["nve", "dai"]);
+    }
 }

tools/dict-convert/src/lexicon/tone.rs
@@ -1,6 +1,8 @@
 //! 拼音写法归一：带声调符号的（Unihan 的 `xíng`）与带声调数字的（通用词表的 `wei4'shen2'me`）都转成
 //! 不带声调、ü 写 v 的小写音节。
 
+use glimmer_dictionary::canonical_syllable;
+
 /// `xíng` → `xing`，`lǜ` → `lv`，`ê̄` 这类罕见记号转成对应字母；组合用变音符号（U+0300–U+036F）丢掉。
 pub fn strip_tone(reading: &str) -> String {
     let mut out = String::with_capacity(reading.len());
@@ -20,7 +22,7 @@ pub fn strip_tone(reading: &str) -> String {
         };
         out.push(plain);
     }
-    out
+    canonical_syllable(&out).to_owned()
 }
 
 /// 通用词表的 `wei4'shen2'me` → `["wei", "shen", "me"]`；`lv4` 保持 v。空段跳过。
@@ -42,6 +44,10 @@ mod tests {
         assert_eq!(strip_tone("xíng"), "xing");
         assert_eq!(strip_tone("lǜ"), "lv");
         assert_eq!(strip_tone("nǚ"), "nv");
+        assert_eq!(strip_tone("lüè"), "lve");
+        assert_eq!(strip_tone("nüè"), "nve");
+        assert_eq!(strip_tone("lue4"), "lve");
+        assert_eq!(strip_tone("nue4"), "nve");
         assert_eq!(strip_tone("shi4"), "shi");
         assert_eq!(strip_tone("de"), "de");
     }