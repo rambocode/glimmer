1lifedo1thing/qingjian

Windows：安装包并排装 32 位 TSF DLL，修 32 位应用无法输入
企业微信 / WPS / 32 位 QQ 这类 32 位进程加载不了 64 位 DLL，切到青简后聚焦即静默回退到别的输入法。
- build.ps1 多编一份 i686 DLL；qingjian.iss 并排装 qingjian_tsf-<版本>-x86.dll，用 SysWOW64 的 regsvr32
  注册（写进 WOW6432Node 给 32 位进程），升级 / 重装 / 卸载清理都带上。
- .cargo/config.toml 给 i686 目标同样开 crt-static；CI（ci.yml + release.yml）加 i686 target 与一个 32 位 check。
- CHANGELOG 与 installer/README 各补一条。

Closes #76
Closes #13

apps/windows/installer/build.ps1
@@ -3,7 +3,7 @@
     在 Windows 上打青简安装包：release 构建三个产物 + 用 Inno Setup 编 qingjian.iss。
 .DESCRIPTION
     在编译机（MSVC 工具链 + Inno Setup）上跑。步骤：
-      1) cargo build --release 出 DLL / Server / 设置程序；
+      1) cargo build --release 出 DLL / Server / 设置程序，再单独编一份 32 位 DLL（企业微信 / WPS 这类 32 位应用只能加载 32 位 DLL）；
       2) 从 apps\windows\server\Cargo.toml 读版本号（-dev 版接 git 短哈希，工作区有改动再加 +，与 macOS 的 bundle.sh 一致）；
       3) 找 ISCC.exe（PATH 或常见安装位置）；
       4) iscc /DAppVersion=<版本> 编脚本，成品在 target\installer\Qingjian-<版本>-Setup.exe。
@@ -46,13 +46,21 @@ if (-not $SkipBuild) {
     try {
         cargo build --release --locked -p qingjian-windows-server -p qingjian-windows-tsf -p qingjian-windows-settings
         if ($LASTEXITCODE -ne 0) { throw "cargo build 失败（退出码 $LASTEXITCODE）" }
+        # 32 位 DLL 供企业微信 / WPS / 32 位 QQ 这类 32 位应用加载。
+        cargo build --release --locked -p qingjian-windows-tsf --target i686-pc-windows-msvc
+        if ($LASTEXITCODE -ne 0) { throw "32 位 DLL cargo build 失败（退出码 $LASTEXITCODE）" }
     } finally { Pop-Location }
 }
 
 # 缺一个产物就早报错。
-$targets = @('qingjian_tsf.dll', 'qingjian-server.exe', 'qingjian-settings.exe')
+$targets = @(
+    'release\qingjian_tsf.dll',
+    'i686-pc-windows-msvc\release\qingjian_tsf.dll',
+    'release\qingjian-server.exe',
+    'release\qingjian-settings.exe'
+)
 foreach ($t in $targets) {
-    $p = Join-Path $Repo "target\release\$t"
+    $p = Join-Path $Repo "target\$t"
     if (-not (Test-Path $p)) { throw "缺产物 $p，先跑一次不带 -SkipBuild 的构建" }
 }
 
@@ -81,7 +89,7 @@ Write-Host "自包含运行时 $($wanted.Count) 项 → target\installer\setting
 # 1.5) 签名（必须在 iscc 打包前：Inno 把已签的文件原样拷进安装包）。
 if ($Sign) {
     Write-Host '自签产物（uiAccess 要求 Server 代码签名）…' -ForegroundColor Cyan
-    $binaries = $targets | ForEach-Object { Join-Path $Repo "target\release\$_" }
+    $binaries = $targets | ForEach-Object { Join-Path $Repo "target\$_" }
     & (Join-Path $PSScriptRoot 'sign-local.ps1') -Path $binaries
 }
 

rambocode/glimmer

企业微信 / WPS / 32 位 QQ 这类 32 位进程加载不了 64 位 DLL，切到微明后聚焦即静默回退到别的输入法。
- build.ps1 多编一份 i686 DLL；glimmer.iss 并排装 glimmer_tsf-<版本>-x86.dll，用 SysWOW64 的 regsvr32
(cherry picked from commit 8bd20205ddf8e45a5c0657e7952fc35fdbdebc0d)

apps/windows/installer/build.ps1
@@ -3,7 +3,7 @@
     在 Windows 上打微明安装包：release 构建三个产物 + 用 Inno Setup 编 glimmer.iss。
 .DESCRIPTION
     在编译机（MSVC 工具链 + Inno Setup）上跑。步骤：
-      1) cargo build --release 出 DLL / Server / 设置程序；
+      1) cargo build --release 出 DLL / Server / 设置程序，再单独编一份 32 位 DLL（企业微信 / WPS 这类 32 位应用只能加载 32 位 DLL）；
       2) 从 apps\windows\server\Cargo.toml 读版本号（-dev 版接 git 短哈希，工作区有改动再加 +，与 macOS 的 bundle.sh 一致）；
       3) 找 ISCC.exe（PATH 或常见安装位置）；
       4) iscc /DAppVersion=<版本> 编脚本，成品在 target\installer\Glimmer-<版本>-Setup.exe。
@@ -46,13 +46,21 @@ if (-not $SkipBuild) {
     try {
         cargo build --release --locked -p glimmer-windows-server -p glimmer-windows-tsf -p glimmer-windows-settings
         if ($LASTEXITCODE -ne 0) { throw "cargo build 失败（退出码 $LASTEXITCODE）" }
+        # 32 位 DLL 供企业微信 / WPS / 32 位 QQ 这类 32 位应用加载。
+        cargo build --release --locked -p glimmer-windows-tsf --target i686-pc-windows-msvc
+        if ($LASTEXITCODE -ne 0) { throw "32 位 DLL cargo build 失败（退出码 $LASTEXITCODE）" }
     } finally { Pop-Location }
 }
 
 # 缺一个产物就早报错。
-$targets = @('glimmer_tsf.dll', 'glimmer-server.exe', 'glimmer-settings.exe')
+$targets = @(
+    'release\glimmer_tsf.dll',
+    'i686-pc-windows-msvc\release\glimmer_tsf.dll',
+    'release\glimmer-server.exe',
+    'release\glimmer-settings.exe'
+)
 foreach ($t in $targets) {
-    $p = Join-Path $Repo "target\release\$t"
+    $p = Join-Path $Repo "target\$t"
     if (-not (Test-Path $p)) { throw "缺产物 $p，先跑一次不带 -SkipBuild 的构建" }
 }
 
@@ -81,7 +89,7 @@ Write-Host "自包含运行时 $($wanted.Count) 项 → target\installer\setting
 # 1.5) 签名（必须在 iscc 打包前：Inno 把已签的文件原样拷进安装包）。
 if ($Sign) {
     Write-Host '自签产物（uiAccess 要求 Server 代码签名）…' -ForegroundColor Cyan
-    $binaries = $targets | ForEach-Object { Join-Path $Repo "target\release\$_" }
+    $binaries = $targets | ForEach-Object { Join-Path $Repo "target\$_" }
     & (Join-Path $PSScriptRoot 'sign-local.ps1') -Path $binaries
 }
 