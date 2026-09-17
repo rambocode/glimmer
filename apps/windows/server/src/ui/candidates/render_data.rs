1lifedo1thing/qingjian

渲染器：Windows 壳接自绘渲染器（候选窗口 + 悬浮状态条）+ 设置页「渲染引擎 / 字体」
- qingjian-render：render_status（StatusCell::{Text, Gear}，返回位图与各格右边界供点击命中）；
  齿轮矢量画（Segoe UI Emoji 排在回退链前面会把 ⚙ 画成彩色）；fonts/directwrite（cfg windows，
  按字族名找字体文件、列字族名）；preview 例子多出 status-light / dark.png。
- Server：ui/painter 一份渲染器，候选窗口与状态条共用（字体库与字形缓存一份）；layered::present 把预乘 RGBA
  换成 BGRA 贴分层窗口，阴影由渲染器画（Shadow::mac_panel()）。[general] renderer / font 经
  CandidateSink::configure 送到 UI 线程，装上时与热加载变了时各送一次；renderer = "system" 或字体库
  建不起来退回原来的 GDI 画法。候选行直接用渲染器的 Row / Tone，本地那份删掉。
- 设置程序：候选窗口页加「渲染引擎」下拉与「字体」提示框（DirectWrite 列字族，正好等于某字族或清空才落盘）。
- 真机（Windows 11 26200）验过：深浅色 / 竖横排 / emoji 彩色 / 阴影 / 字体热切换 / 状态条 / GDI 退路；
  结果记在 docs/design/rendering.md「Windows 半边」。

apps/windows/server/src/ui/candidates/render_data.rs
@@ -1,11 +1,12 @@
-//! 候选窗口一次绘制要用的全部内容，由帧换算而来。
+//! 候选窗口一次绘制要用的全部内容，由帧换算而来；渲染器要的帧由 [`RenderData::render_frame`] 再换一次。
 
 use std::rc::Rc;
 
 use qingjian_platform::protocol::{Frame, PreeditKind};
 use qingjian_platform::{LayoutMode, ThemeMode};
+use qingjian_render::{Preedit, PreeditSegment, PreeditStyle, Row};
 
-use super::row::Row;
+use super::row;
 use super::theme::Theme;
 
 /// 一次绘制要用的全部内容。
@@ -71,12 +72,39 @@ impl RenderData {
             .items
             .iter()
             .enumerate()
-            .map(|(i, candidate)| Row::from_candidate(i, candidate))
+            .map(|(i, candidate)| row::from_candidate(i, candidate))
             .collect();
         self.highlight = frame.highlight;
         self.footer =
             (frame.page_count > 1).then(|| format!("{}/{}", frame.page + 1, frame.page_count));
         self.sentence = frame.sentence.clone();
         self.notice = frame.notice.clone();
+    }
+
+    /// 渲染器要的帧。提示（删了什么词）在渲染器里画在拼音行右侧，与 macOS 一致。
+    pub(super) fn render_frame(&self) -> qingjian_render::Frame {
+        let preedit = (!self.preedit.is_empty()).then(|| Preedit {
+            segments: self
+                .preedit
+                .iter()
+                .map(|(text, kind)| PreeditSegment {
+                    text: text.clone(),
+                    style: match kind {
+                        PreeditKind::Typed => PreeditStyle::Typed,
+                        PreeditKind::Rest => PreeditStyle::Rest,
+                        PreeditKind::Corrected => PreeditStyle::Struck,
+                    },
+                })
+                .collect(),
+            cursor: self.cursor,
+        });
+        qingjian_render::Frame {
+            preedit,
+            rows: self.rows.clone(),
+            highlighted: self.highlight,
+            footer: self.footer.clone(),
+            sentence: self.sentence.clone(),
+            status: self.notice.clone(),
+        }
     }
 }

apps/windows/server/src/ui/candidates/view.rs
@@ -3,6 +3,7 @@
 
 use qingjian_platform::LayoutMode;
 use qingjian_platform::protocol::PreeditKind;
+use qingjian_render::{Row, Tone};
 use windows::Win32::Foundation::{COLORREF, RECT, SIZE};
 use windows::Win32::Graphics::Gdi::{
     CreateRoundRectRgn, CreateSolidBrush, DeleteObject, FillRect, FillRgn, GetTextExtentPoint32W,
@@ -10,7 +11,6 @@ use windows::Win32::Graphics::Gdi::{
 };
 
 use super::RenderData;
-use super::row::{Row, Tone};
 use super::theme::Theme;
 
 /// 云端候选词前的小云朵（macOS 用 SF Symbol `cloud`）。

apps/windows/server/src/ui/layered/mod.rs
@@ -1,9 +1,11 @@
 //! 分层窗口合成：圆角背景 + 四周柔和阴影 + 一段 GDI 内容，合成进一张预乘 alpha 的 BGRA 位图，
 //! `UpdateLayeredWindow` 一次贴上。候选窗口与状态条共用。位图在 [`Canvas`]，内容圆角矩形在 [`RoundRect`]。
+//! 青简渲染器画好的整张位图（已含阴影）走 [`present`]，只做 RGBA → BGRA 再贴。
 
 mod canvas;
 mod round_rect;
 
+use qingjian_render::Pixmap;
 use windows::Win32::Foundation::{COLORREF, E_INVALIDARG, HWND, POINT, RECT, SIZE};
 use windows::Win32::Graphics::Gdi::{
     AC_SRC_ALPHA, AC_SRC_OVER, BLENDFUNCTION, HDC, SetViewportOrgEx,
@@ -86,10 +88,32 @@ pub(super) fn composite(hwnd: HWND, layered: &Layered) -> Result<()> {
     }
     // GDI 只写 RGB、把碰到的像素 alpha 留成 0（分层窗口里会全透明），画完把内容区补回 255。
     restore_content_alpha(canvas.pixels(), w, h, &round);
+    update(hwnd, canvas.dc(), layered.win_pos, (w, h))
+}
 
+/// 把渲染器出的预乘 RGBA 位图（已含阴影边）贴到分层窗口上，`win_pos` 是位图左上角的屏幕坐标。
+pub(super) fn present(hwnd: HWND, pixmap: &Pixmap, win_pos: (i32, i32)) -> Result<()> {
+    let (w, h) = (pixmap.width() as i32, pixmap.height() as i32);
+    if w <= 0 || h <= 0 {
+        return Err(Error::from(E_INVALIDARG));
+    }
+    let mut canvas = Canvas::new(w, h)?;
+    // tiny-skia 是 RGBA，DIB 是 BGRA；都是预乘，只换通道顺序。
+    for (dst, src) in canvas.pixels().chunks_exact_mut(4).zip(pixmap.pixels()) {
+        dst[0] = src.blue();
+        dst[1] = src.green();
+        dst[2] = src.red();
+        dst[3] = src.alpha();
+    }
+    update(hwnd, canvas.dc(), win_pos, (w, h))
+}
+
+/// `UpdateLayeredWindow`：整张位图按预乘 alpha 贴上并挪到 `win_pos`。
+fn update(hwnd: HWND, hdc: HDC, win_pos: (i32, i32), win_size: (i32, i32)) -> Result<()> {
+    let (w, h) = win_size;
     let dst = POINT {
-        x: layered.win_pos.0,
-        y: layered.win_pos.1,
+        x: win_pos.0,
+        y: win_pos.1,
     };
     let size = SIZE { cx: w, cy: h };
     let src = POINT { x: 0, y: 0 };
@@ -105,7 +129,7 @@ pub(super) fn composite(hwnd: HWND, layered: &Layered) -> Result<()> {
             None,
             Some(&dst),
             Some(&size),
-            Some(canvas.dc()),
+            Some(hdc),
             Some(&src),
             COLORREF(0),
             Some(&blend),

apps/windows/server/src/ui/mod.rs
@@ -8,9 +8,12 @@ mod candidates;
 mod command;
 mod layered;
 mod monitor;
+mod painter;
 mod status;
 mod window_class;
 
+use std::cell::RefCell;
+use std::rc::Rc;
 use std::sync::mpsc::{self, Receiver, Sender};
 use std::thread;
 
@@ -29,8 +32,9 @@ use qingjian_platform::protocol::{Frame, ScreenRect};
 
 use self::candidates::CandidateWindow;
 use self::command::UiCommand;
+use self::painter::{Painter, SharedPainter};
 use self::status::StatusBar;
-use crate::dispatch::{CandidateSink, StatusEvent, StatusSink, StatusView};
+use crate::dispatch::{CandidateSink, RenderSettings, StatusEvent, StatusSink, StatusView};
 
 /// 状态条上的操作（点格子 / 拖动结束）回给 Router 的回调，UI 线程上调。
 pub type StatusEvents = Box<dyn Fn(StatusEvent) + Send>;
@@ -83,6 +87,10 @@ impl CandidateSink for UiHandle {
     fn hide(&self) {
         self.post(UiCommand::Hide);
     }
+
+    fn configure(&self, settings: RenderSettings) {
+        self.post(UiCommand::Configure(settings));
+    }
 }
 
 impl StatusSink for UiHandle {
@@ -106,8 +114,10 @@ fn run(commands: Receiver<UiCommand>, ready: &Sender<Option<u32>>, on_status: St
     // 按物理像素定位，与应用报来的组句屏幕矩形对齐；已设过会失败，忽略。
     let _ = unsafe { SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2) };
     let thread_id = unsafe { GetCurrentThreadId() };
+    // 渲染器候选窗口与状态条共用（字体库与字形缓存一份）；装上时按配置建。
+    let painter: SharedPainter = Rc::new(RefCell::new(None));
     // 先建窗口再报 id：建窗口顺带建起本线程的消息队列，之后 PostThreadMessageW 才有处可投。
-    let window = match CandidateWindow::new() {
+    let window = match CandidateWindow::new(painter.clone()) {
         Ok(window) => window,
         Err(error) => {
             tracing::error!(%error, "建候选窗口失败，Server 将不显示候选框");
@@ -115,7 +125,7 @@ fn run(commands: Receiver<UiCommand>, ready: &Sender<Option<u32>>, on_status: St
             return;
         }
     };
-    let status = match StatusBar::new(on_status) {
+    let status = match StatusBar::new(on_status, painter.clone()) {
         Ok(status) => Some(status),
         Err(error) => {
             tracing::error!(%error, "建悬浮状态条失败，将不显示状态条");
@@ -134,7 +144,7 @@ fn run(commands: Receiver<UiCommand>, ready: &Sender<Option<u32>>, on_status: St
         if msg.message == WM_WAKE {
             // 一次唤醒排空整个队列，保住 Hide→Show 的先后。
             while let Ok(command) = commands.try_recv() {
-                apply(&window, status.as_ref(), command);
+                apply(&window, status.as_ref(), &painter, command);
             }
             continue;
         }
@@ -145,7 +155,12 @@ fn run(commands: Receiver<UiCommand>, ready: &Sender<Option<u32>>, on_status: St
     }
 }
 
-fn apply(window: &CandidateWindow, status: Option<&StatusBar>, command: UiCommand) {
+fn apply(
+    window: &CandidateWindow,
+    status: Option<&StatusBar>,
+    painter: &SharedPainter,
+    command: UiCommand,
+) {
     match command {
         UiCommand::Show(payload) => {
             let (frame, rect) = *payload;
@@ -163,6 +178,7 @@ fn apply(window: &CandidateWindow, status: Option<&StatusBar>, command: UiComman
                 status.hide();
             }
         }
+        UiCommand::Configure(settings) => Painter::configure(painter, &settings),
     }
 }
 

apps/windows/settings/src/panel/component.rs
@@ -1,7 +1,8 @@
 //! 根组件的 Reactor 生命周期：建状态、按消息落盘、画左侧导航 + 当前页。
 
 use qingjian_platform::{
-    Config, DEFAULT_ENGLISH_CANDIDATES_OFF_WINDOWS, LayoutMode, LogLevel, PreeditMode, ThemeMode,
+    CandidateRenderer, Config, DEFAULT_ENGLISH_CANDIDATES_OFF_WINDOWS, LayoutMode, LogLevel,
+    PreeditMode, ThemeMode,
 };
 use windows_reactor::*;
 
@@ -22,6 +23,8 @@ impl Component for Settings {
             path,
             page: "general".to_string(),
             cloud_status: CloudStatus::Idle,
+            families: qingjian_render::system_fonts::families(),
+            font_query: None,
         }
     }
 
@@ -70,6 +73,32 @@ impl Component for Settings {
             }
             Message::Preedit(Some(i)) if i < PreeditMode::ALL.len() => {
                 self.save("general", "preedit", PreeditMode::ALL[i].key());
+            }
+            Message::Renderer(Some(i)) if i < CandidateRenderer::ALL.len() => {
+                self.save("general", "renderer", CandidateRenderer::ALL[i].key());
+            }
+            Message::FontQuery(text) => {
+                let text = text.trim().to_owned();
+                let exact = self
+                    .families
+                    .iter()
+                    .find(|family| family.eq_ignore_ascii_case(&text))
+                    .cloned();
+                match exact {
+                    Some(family) => {
+                        self.font_query = None;
+                        self.save("general", "font", family);
+                    }
+                    None if text.is_empty() => {
+                        self.font_query = None;
+                        self.save("general", "font", "");
+                    }
+                    None => self.font_query = Some(text),
+                }
+            }
+            Message::Font(family) => {
+                self.font_query = None;
+                self.save("general", "font", family);
             }
             Message::StatusBar(on) => self.save("status_bar", "enabled", on),
 

apps/windows/settings/src/panel/pages/candidates.rs
@@ -1,6 +1,6 @@
-//! 「候选窗口」页：外观、排布、拼音显示位置、悬浮状态条。
+//! 「候选窗口」页：外观、排布、渲染引擎、字体、拼音显示位置、悬浮状态条。
 
-use qingjian_platform::{LayoutMode, PreeditMode, ThemeMode};
+use qingjian_platform::{CandidateRenderer, LayoutMode, PreeditMode, ThemeMode};
 use windows_reactor::*;
 
 use crate::panel::controls::{field, page};
@@ -21,6 +21,17 @@ fn mode_combo<T: PartialEq + Copy>(
 
 pub(crate) fn view(settings: &Settings, context: &mut ViewContext<Settings>) -> View {
     let g = &settings.config.general;
+    let font_text = settings
+        .font_query
+        .clone()
+        .unwrap_or_else(|| g.font.clone());
+    let query = font_text.to_lowercase();
+    let suggestions: Vec<String> = settings
+        .families
+        .iter()
+        .filter(|family| query.is_empty() || family.to_lowercase().contains(&query))
+        .cloned()
+        .collect();
     let rows = [
         field(
             "外观",
@@ -41,6 +52,27 @@ pub(crate) fn view(settings: &Settings, context: &mut ViewContext<Settings>) -> 
                 LayoutMode::label,
                 context.callback(Message::Layout),
             ),
+        ),
+        field(
+            "渲染引擎",
+            "青简渲染器让候选窗口在各平台一致。",
+            mode_combo(
+                &CandidateRenderer::ALL,
+                g.renderer,
+                CandidateRenderer::label,
+                context.callback(Message::Renderer),
+            ),
+        ),
+        field(
+            "字体",
+            "只对青简渲染器生效；留空用系统字体，没装的字体自动回到系统字体。",
+            AutoSuggestBox::new()
+                .width(260.0)
+                .text(font_text)
+                .placeholder_text("系统字体")
+                .items_source(suggestions)
+                .on_text_changed(context.callback(Message::FontQuery))
+                .on_suggestion_chosen(context.callback(Message::Font)),
         ),
         field(
             "拼音显示",

rambocode/glimmer

- glimmer-render：render_status（StatusCell::{Text, Gear}，返回位图与各格右边界供点击命中）；
(cherry picked from commit c26f51af6ff0842aa01376ed9e4e53225e23d568)

apps/windows/server/src/ui/candidates/render_data.rs
@@ -1,11 +1,12 @@
-//! 候选窗口一次绘制要用的全部内容，由帧换算而来。
+//! 候选窗口一次绘制要用的全部内容，由帧换算而来；渲染器要的帧由 [`RenderData::render_frame`] 再换一次。
 
 use std::rc::Rc;
 
 use glimmer_platform::protocol::{Frame, PreeditKind};
 use glimmer_platform::{LayoutMode, ThemeMode};
+use glimmer_render::{Preedit, PreeditSegment, PreeditStyle, Row};
 
-use super::row::Row;
+use super::row;
 use super::theme::Theme;
 
 /// 一次绘制要用的全部内容。
@@ -71,12 +72,39 @@ impl RenderData {
             .items
             .iter()
             .enumerate()
-            .map(|(i, candidate)| Row::from_candidate(i, candidate))
+            .map(|(i, candidate)| row::from_candidate(i, candidate))
             .collect();
         self.highlight = frame.highlight;
         self.footer =
             (frame.page_count > 1).then(|| format!("{}/{}", frame.page + 1, frame.page_count));
         self.sentence = frame.sentence.clone();
         self.notice = frame.notice.clone();
+    }
+
+    /// 渲染器要的帧。提示（删了什么词）在渲染器里画在拼音行右侧，与 macOS 一致。
+    pub(super) fn render_frame(&self) -> glimmer_render::Frame {
+        let preedit = (!self.preedit.is_empty()).then(|| Preedit {
+            segments: self
+                .preedit
+                .iter()
+                .map(|(text, kind)| PreeditSegment {
+                    text: text.clone(),
+                    style: match kind {
+                        PreeditKind::Typed => PreeditStyle::Typed,
+                        PreeditKind::Rest => PreeditStyle::Rest,
+                        PreeditKind::Corrected => PreeditStyle::Struck,
+                    },
+                })
+                .collect(),
+            cursor: self.cursor,
+        });
+        glimmer_render::Frame {
+            preedit,
+            rows: self.rows.clone(),
+            highlighted: self.highlight,
+            footer: self.footer.clone(),
+            sentence: self.sentence.clone(),
+            status: self.notice.clone(),
+        }
     }
 }

apps/windows/server/src/ui/candidates/view.rs
@@ -3,6 +3,7 @@
 
 use glimmer_platform::LayoutMode;
 use glimmer_platform::protocol::PreeditKind;
+use glimmer_render::{Row, Tone};
 use windows::Win32::Foundation::{COLORREF, RECT, SIZE};
 use windows::Win32::Graphics::Gdi::{
     CreateRoundRectRgn, CreateSolidBrush, DeleteObject, FillRect, FillRgn, GetTextExtentPoint32W,
@@ -10,7 +11,6 @@ use windows::Win32::Graphics::Gdi::{
 };
 
 use super::RenderData;
-use super::row::{Row, Tone};
 use super::theme::Theme;
 
 /// 云端候选词前的小云朵（macOS 用 SF Symbol `cloud`）。

apps/windows/server/src/ui/layered/mod.rs
@@ -1,9 +1,11 @@
 //! 分层窗口合成：圆角背景 + 四周柔和阴影 + 一段 GDI 内容，合成进一张预乘 alpha 的 BGRA 位图，
 //! `UpdateLayeredWindow` 一次贴上。候选窗口与状态条共用。位图在 [`Canvas`]，内容圆角矩形在 [`RoundRect`]。
+//! 微明渲染器画好的整张位图（已含阴影）走 [`present`]，只做 RGBA → BGRA 再贴。
 
 mod canvas;
 mod round_rect;
 
+use glimmer_render::Pixmap;
 use windows::Win32::Foundation::{COLORREF, E_INVALIDARG, HWND, POINT, RECT, SIZE};
 use windows::Win32::Graphics::Gdi::{
     AC_SRC_ALPHA, AC_SRC_OVER, BLENDFUNCTION, HDC, SetViewportOrgEx,
@@ -86,10 +88,32 @@ pub(super) fn composite(hwnd: HWND, layered: &Layered) -> Result<()> {
     }
     // GDI 只写 RGB、把碰到的像素 alpha 留成 0（分层窗口里会全透明），画完把内容区补回 255。
     restore_content_alpha(canvas.pixels(), w, h, &round);
+    update(hwnd, canvas.dc(), layered.win_pos, (w, h))
+}
 
+/// 把渲染器出的预乘 RGBA 位图（已含阴影边）贴到分层窗口上，`win_pos` 是位图左上角的屏幕坐标。
+pub(super) fn present(hwnd: HWND, pixmap: &Pixmap, win_pos: (i32, i32)) -> Result<()> {
+    let (w, h) = (pixmap.width() as i32, pixmap.height() as i32);
+    if w <= 0 || h <= 0 {
+        return Err(Error::from(E_INVALIDARG));
+    }
+    let mut canvas = Canvas::new(w, h)?;
+    // tiny-skia 是 RGBA，DIB 是 BGRA；都是预乘，只换通道顺序。
+    for (dst, src) in canvas.pixels().chunks_exact_mut(4).zip(pixmap.pixels()) {
+        dst[0] = src.blue();
+        dst[1] = src.green();
+        dst[2] = src.red();
+        dst[3] = src.alpha();
+    }
+    update(hwnd, canvas.dc(), win_pos, (w, h))
+}
+
+/// `UpdateLayeredWindow`：整张位图按预乘 alpha 贴上并挪到 `win_pos`。
+fn update(hwnd: HWND, hdc: HDC, win_pos: (i32, i32), win_size: (i32, i32)) -> Result<()> {
+    let (w, h) = win_size;
     let dst = POINT {
-        x: layered.win_pos.0,
-        y: layered.win_pos.1,
+        x: win_pos.0,
+        y: win_pos.1,
     };
     let size = SIZE { cx: w, cy: h };
     let src = POINT { x: 0, y: 0 };
@@ -105,7 +129,7 @@ pub(super) fn composite(hwnd: HWND, layered: &Layered) -> Result<()> {
             None,
             Some(&dst),
             Some(&size),
-            Some(canvas.dc()),
+            Some(hdc),
             Some(&src),
             COLORREF(0),
             Some(&blend),

apps/windows/server/src/ui/mod.rs
@@ -8,9 +8,12 @@ mod candidates;
 mod command;
 mod layered;
 mod monitor;
+mod painter;
 mod status;
 mod window_class;
 
+use std::cell::RefCell;
+use std::rc::Rc;
 use std::sync::mpsc::{self, Receiver, Sender};
 use std::thread;
 
@@ -26,10 +29,11 @@ use windows::Win32::UI::WindowsAndMessaging::{
 use windows::core::{Error, Result};
 
 use glimmer_platform::protocol::{Frame, ScreenRect};
-use glimmer_server::dispatch::{CandidateSink, StatusEvent, StatusSink, StatusView};
+use glimmer_server::dispatch::{CandidateSink, RenderSettings, StatusEvent, StatusSink, StatusView};
 
 use self::candidates::CandidateWindow;
 use self::command::UiCommand;
+use self::painter::{Painter, SharedPainter};
 use self::status::StatusBar;
 
 /// 状态条上的操作（点格子 / 拖动结束）回给 Router 的回调，UI 线程上调。
@@ -83,6 +87,10 @@ impl CandidateSink for UiHandle {
     fn hide(&self) {
         self.post(UiCommand::Hide);
     }
+
+    fn configure(&self, settings: RenderSettings) {
+        self.post(UiCommand::Configure(settings));
+    }
 }
 
 impl StatusSink for UiHandle {
@@ -106,8 +114,10 @@ fn run(commands: Receiver<UiCommand>, ready: &Sender<Option<u32>>, on_status: St
     // 按物理像素定位，与应用报来的组句屏幕矩形对齐；已设过会失败，忽略。
     let _ = unsafe { SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2) };
     let thread_id = unsafe { GetCurrentThreadId() };
+    // 渲染器候选窗口与状态条共用（字体库与字形缓存一份）；装上时按配置建。
+    let painter: SharedPainter = Rc::new(RefCell::new(None));
     // 先建窗口再报 id：建窗口顺带建起本线程的消息队列，之后 PostThreadMessageW 才有处可投。
-    let window = match CandidateWindow::new() {
+    let window = match CandidateWindow::new(painter.clone()) {
         Ok(window) => window,
         Err(error) => {
             tracing::error!(%error, "建候选窗口失败，Server 将不显示候选框");
@@ -115,7 +125,7 @@ fn run(commands: Receiver<UiCommand>, ready: &Sender<Option<u32>>, on_status: St
             return;
         }
     };
-    let status = match StatusBar::new(on_status) {
+    let status = match StatusBar::new(on_status, painter.clone()) {
         Ok(status) => Some(status),
         Err(error) => {
             tracing::error!(%error, "建悬浮状态条失败，将不显示状态条");
@@ -134,7 +144,7 @@ fn run(commands: Receiver<UiCommand>, ready: &Sender<Option<u32>>, on_status: St
         if msg.message == WM_WAKE {
             // 一次唤醒排空整个队列，保住 Hide→Show 的先后。
             while let Ok(command) = commands.try_recv() {
-                apply(&window, status.as_ref(), command);
+                apply(&window, status.as_ref(), &painter, command);
             }
             continue;
         }
@@ -145,7 +155,12 @@ fn run(commands: Receiver<UiCommand>, ready: &Sender<Option<u32>>, on_status: St
     }
 }
 
-fn apply(window: &CandidateWindow, status: Option<&StatusBar>, command: UiCommand) {
+fn apply(
+    window: &CandidateWindow,
+    status: Option<&StatusBar>,
+    painter: &SharedPainter,
+    command: UiCommand,
+) {
     match command {
         UiCommand::Show(payload) => {
             let (frame, rect) = *payload;
@@ -163,6 +178,7 @@ fn apply(window: &CandidateWindow, status: Option<&StatusBar>, command: UiComman
                 status.hide();
             }
         }
+        UiCommand::Configure(settings) => Painter::configure(painter, &settings),
     }
 }
 

apps/windows/settings/src/panel/component.rs
@@ -1,8 +1,8 @@
 //! 根组件的 Reactor 生命周期：建状态、按消息落盘、画左侧导航 + 当前页。
 
 use glimmer_platform::{
-    Config, DEFAULT_ENGLISH_CANDIDATES_OFF_WINDOWS, LayoutMode, LogLevel, PreeditMode,
-    PunctuationMode, ThemeMode,
+    CandidateRenderer, Config, DEFAULT_ENGLISH_CANDIDATES_OFF_WINDOWS, LayoutMode, LogLevel,
+    PreeditMode, PunctuationMode, ThemeMode,
 };
 use windows_reactor::*;
 
@@ -23,6 +23,8 @@ impl Component for Settings {
             path,
             page: "general".to_string(),
             cloud_status: CloudStatus::Idle,
+            families: glimmer_render::system_fonts::families(),
+            font_query: None,
         }
     }
 
@@ -79,6 +81,32 @@ impl Component for Settings {
             }
             Message::Preedit(Some(i)) if i < PreeditMode::ALL.len() => {
                 self.save("general", "preedit", PreeditMode::ALL[i].key());
+            }
+            Message::Renderer(Some(i)) if i < CandidateRenderer::ALL.len() => {
+                self.save("general", "renderer", CandidateRenderer::ALL[i].key());
+            }
+            Message::FontQuery(text) => {
+                let text = text.trim().to_owned();
+                let exact = self
+                    .families
+                    .iter()
+                    .find(|family| family.eq_ignore_ascii_case(&text))
+                    .cloned();
+                match exact {
+                    Some(family) => {
+                        self.font_query = None;
+                        self.save("general", "font", family);
+                    }
+                    None if text.is_empty() => {
+                        self.font_query = None;
+                        self.save("general", "font", "");
+                    }
+                    None => self.font_query = Some(text),
+                }
+            }
+            Message::Font(family) => {
+                self.font_query = None;
+                self.save("general", "font", family);
             }
             Message::StatusBar(on) => self.save("status_bar", "enabled", on),
 

apps/windows/settings/src/panel/pages/candidates.rs
@@ -1,6 +1,6 @@
-//! 「候选窗口」页：外观、排布、拼音显示位置、悬浮状态条。
+//! 「候选窗口」页：外观、排布、渲染引擎、字体、拼音显示位置、悬浮状态条。
 
-use glimmer_platform::{LayoutMode, PreeditMode, ThemeMode};
+use glimmer_platform::{CandidateRenderer, LayoutMode, PreeditMode, ThemeMode};
 use windows_reactor::*;
 
 use crate::panel::controls::{field, page};
@@ -21,6 +21,17 @@ fn mode_combo<T: PartialEq + Copy>(
 
 pub(crate) fn view(settings: &Settings, context: &mut ViewContext<Settings>) -> View {
     let g = &settings.config.general;
+    let font_text = settings
+        .font_query
+        .clone()
+        .unwrap_or_else(|| g.font.clone());
+    let query = font_text.to_lowercase();
+    let suggestions: Vec<String> = settings
+        .families
+        .iter()
+        .filter(|family| query.is_empty() || family.to_lowercase().contains(&query))
+        .cloned()
+        .collect();
     let rows = [
         field(
             "外观",
@@ -41,6 +52,27 @@ pub(crate) fn view(settings: &Settings, context: &mut ViewContext<Settings>) -> 
                 LayoutMode::label,
                 context.callback(Message::Layout),
             ),
+        ),
+        field(
+            "渲染引擎",
+            "微明渲染器让候选窗口在各平台一致。",
+            mode_combo(
+                &CandidateRenderer::ALL,
+                g.renderer,
+                CandidateRenderer::label,
+                context.callback(Message::Renderer),
+            ),
+        ),
+        field(
+            "字体",
+            "只对微明渲染器生效；留空用系统字体，没装的字体自动回到系统字体。",
+            AutoSuggestBox::new()
+                .width(260.0)
+                .text(font_text)
+                .placeholder_text("系统字体")
+                .items_source(suggestions)
+                .on_text_changed(context.callback(Message::FontQuery))
+                .on_suggestion_chosen(context.callback(Message::Font)),
         ),
         field(
             "拼音显示",