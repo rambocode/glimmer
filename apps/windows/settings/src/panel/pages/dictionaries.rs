1lifedo1thing/qingjian

feat(windows): 批量导入 Rime 词库并自动热加载 (#118)
设置页导入改走 qingjian-dictionary::import 转成 .qj（原来只复制 yaml，Server 不装），多选批量、自动启用、页面显示结果；Server 每次轮询比对 dicts\ 快照，新增 / 同名更新 / 移除不用重启；空词库拒绝导入。修 #36。

apps/windows/settings/src/panel/pages/dictionaries.rs
@@ -150,12 +150,13 @@ pub(crate) fn view(settings: &Settings, context: &mut ViewContext<Settings>) -> 
                     .content("导入词库…"),
                 note("接受青简 TSV、Rime .dict.yaml、.qj；导入即复制进上面的目录。"),
             )),
+        note(&settings.dictionary_status),
     ]);
     page("词库", body)
 }
 
 /// 挪进 `dicts\removed`，不真删（与 macOS 一致）。
-pub(crate) fn remove_user_dict(settings: &Settings, stem: &str) {
+pub(crate) fn remove_user_dict(settings: &mut Settings, stem: &str) {
     let dir = user_dir(settings);
     let Some((_, path)) = extra_dictionaries::list(&dir)
         .into_iter()
@@ -165,35 +166,71 @@ pub(crate) fn remove_user_dict(settings: &Settings, stem: &str) {
     };
     let removed = dir.join("removed");
     if let Err(error) = std::fs::create_dir_all(&removed) {
-        crate::log::warn(format!("建 removed 目录失败: {error}"));
+        settings.dictionary_status = format!("移除失败：{error}");
         return;
     }
-    if let Some(file_name) = path.file_name()
-        && let Err(error) = std::fs::rename(&path, removed.join(file_name))
-    {
-        crate::log::warn(format!("移除词库 {stem} 失败: {error}"));
+    if let Some(file_name) = path.file_name() {
+        settings.dictionary_status = match std::fs::rename(&path, removed.join(file_name)) {
+            Ok(()) => format!("已移除「{stem}」，输入法将自动更新。"),
+            Err(error) => format!("移除失败：{error}"),
+        };
     }
 }
 
-/// 文件选择器选一本，复制进用户词库目录。
-pub(crate) fn import(settings: &Settings) {
-    let Some(source) = rfd::FileDialog::new()
+/// 多选词库，逐个转换并汇总结果；成功项的开关一次写回。
+pub(crate) fn import(settings: &mut Settings) {
+    let Some(sources) = rfd::FileDialog::new()
         .add_filter("词库文件", &["tsv", "yaml", "yml", "qj"])
         .add_filter("所有文件", &["*"])
         .set_title("导入词库")
-        .pick_file()
+        .pick_files()
     else {
         return;
     };
     let dir = user_dir(settings);
-    if let Err(error) = std::fs::create_dir_all(&dir) {
-        crate::log::warn(format!("建用户词库目录失败: {error}"));
-        return;
+    let mut disabled = settings.config.dictionaries.disabled.clone();
+    let mut results = Vec::new();
+    let mut succeeded = 0;
+    for source in &sources {
+        match qingjian_core::dictionary::import::import(source, &dir) {
+            Ok(imported) => {
+                let stem = imported
+                    .path
+                    .file_stem()
+                    .and_then(|s| s.to_str())
+                    .expect("dictionary importer produces a UTF-8 file stem");
+                disabled.retain(|name| name != stem);
+                succeeded += 1;
+                results.push(format!(
+                    "已导入「{}」，共 {} 条。",
+                    imported.name, imported.entries
+                ));
+            }
+            Err(error) => {
+                let message = format!("{} 导入失败：{error}", source.display());
+                crate::log::warn(&message);
+                results.push(message);
+            }
+        }
     }
-    let Some(file_name) = source.file_name() else {
-        return;
-    };
-    if let Err(error) = std::fs::copy(&source, dir.join(file_name)) {
-        crate::log::warn(format!("导入词库失败: {error}"));
+    let mut summary = format!(
+        "导入完成：成功 {} 个，失败 {} 个。",
+        succeeded,
+        sources.len() - succeeded
+    );
+    if disabled != settings.config.dictionaries.disabled
+        && let Err(error) = qingjian_platform::Config::set_array(
+            &settings.path,
+            "dictionaries",
+            "disabled",
+            &disabled,
+        )
+    {
+        results.push(format!(
+            "自动启用失败：{error}。此前关闭的词库需手动勾选启用。"
+        ));
+    } else if succeeded > 0 {
+        summary.push_str("输入法将自动加载。");
     }
+    settings.dictionary_status = format!("{summary}\n{}", results.join("\n"));
 }

apps/windows/server/src/dispatch/reload/state.rs
@@ -4,8 +4,11 @@ use std::path::PathBuf;
 use std::time::{Instant, SystemTime};
 
 use qingjian_core::Language;
-use qingjian_platform::DictionariesConfig;
+use qingjian_dictionary::Dictionary;
+use qingjian_platform::{DictionariesConfig, extra_dictionaries};
 use qingjian_predict::PredictConfig;
+
+use crate::assembly::user_dicts_dir;
 
 /// 热加载状态。
 pub(crate) struct ConfigReload {
@@ -35,4 +38,20 @@ pub(crate) struct ConfigReload {
 
     /// 已应用的学习语言（`None` 为关）。
     pub(super) applied_language: Option<Language>,
+
+    /// 最近加载的用户词库文件快照（路径、修改时间、长度）。
+    pub(super) dictionary_files: Vec<(PathBuf, Option<SystemTime>, u64)>,
+}
+
+impl ConfigReload {
+    /// 按上次有效配置装配词库，不把用户目录中的学习数据当作词库。
+    pub(super) fn load_dictionaries(&self) -> Vec<Dictionary> {
+        let dictionaries = extra_dictionaries::load(
+            self.bundled_dicts_dir.as_deref(),
+            user_dicts_dir(self.user_dir.as_deref()).as_deref(),
+            &self.applied_dictionaries,
+        );
+        tracing::info!(count = dictionaries.len(), "附加词库已热重装");
+        dictionaries
+    }
 }

apps/windows/server/src/dispatch/reload/tests.rs
@@ -1,0 +1,136 @@
+//! 词库文件变化在配置不变时仍应进入正在运行的引擎。
+
+use std::path::Path;
+use std::time::{Duration, Instant, SystemTime};
+
+use qingjian_core::Engine;
+use qingjian_dictionary::{Dictionary, import};
+use qingjian_platform::Config;
+
+use super::CONFIG_POLL_INTERVAL;
+use crate::dispatch::{Router, RouterConfig};
+
+fn poll(router: &mut Router) {
+    router.reload.as_mut().unwrap().last_check = Instant::now() - CONFIG_POLL_INTERVAL;
+    router.poll_config_reload();
+}
+
+fn modified_at(path: &Path, seconds: u64) {
+    std::fs::File::options()
+        .write(true)
+        .open(path)
+        .unwrap()
+        .set_modified(SystemTime::UNIX_EPOCH + Duration::from_secs(seconds))
+        .unwrap();
+}
+
+#[test]
+fn import_replace_and_remove_without_config_changes() {
+    let dir = std::env::temp_dir().join(format!("qingjian-reload-{}", std::process::id()));
+    std::fs::create_dir_all(&dir).unwrap();
+    let config_path = dir.join("config.toml");
+    std::fs::write(&config_path, "").unwrap();
+    let config = Config::load(&config_path).unwrap();
+    let original_config = std::fs::read(&config_path).unwrap();
+    let mut router = Router::new(Engine::new(Dictionary::default()), RouterConfig::default());
+    router.watch_config(&config, config_path.clone(), dir.clone(), Some(dir.clone()));
+
+    let source = dir.join("law.dict.yaml");
+    std::fs::write(&source, "---\nname: law\n...\n合同法\the tong fa\t120\n").unwrap();
+    let imported = import::import(&source, &dir.join("dicts")).unwrap();
+    modified_at(&imported.path, 100);
+    poll(&mut router);
+    let dictionaries = router.engine.extra_dictionaries();
+    assert_eq!(dictionaries.len(), 1);
+    assert_eq!(
+        dictionaries[0].lookup(&["he", "tong", "fa"], false)[0].text,
+        "合同法"
+    );
+
+    // 只改词频，输出长度保持不变，必须靠 mtime 发现更新。
+    let original_len = std::fs::metadata(&imported.path).unwrap().len();
+    std::fs::write(&source, "---\nname: law\n...\n合同法\the tong fa\t121\n").unwrap();
+    import::import(&source, &dir.join("dicts")).unwrap();
+    modified_at(&imported.path, 200);
+    assert_eq!(
+        std::fs::metadata(&imported.path).unwrap().len(),
+        original_len
+    );
+    poll(&mut router);
+    assert_eq!(
+        router.engine.extra_dictionaries()[0].lookup(&["he", "tong", "fa"], false)[0].frequency,
+        121
+    );
+
+    // 保持旧词库映射打开，模拟运行中的 Server 收到同名替换。
+    std::fs::write(
+        &source,
+        "---\nname: law\n...\n民法典\tmin fa dian\t100\n法律\tfa lv\t30\n",
+    )
+    .unwrap();
+    import::import(&source, &dir.join("dicts")).unwrap();
+    poll(&mut router);
+    let dictionary = &router.engine.extra_dictionaries()[0];
+    assert!(dictionary.lookup(&["he", "tong", "fa"], false).is_empty());
+    assert_eq!(
+        dictionary.lookup(&["min", "fa", "dian"], false)[0].text,
+        "民法典"
+    );
+
+    // YAML 头中只有引用、没有本文件词条时，失败不能覆盖已有词库。
+    std::fs::write(&source, "---\nname: law\nimport_tables:\n  - other\n...\n").unwrap();
+    assert!(import::import(&source, &dir.join("dicts")).is_err());
+    assert_eq!(Dictionary::from_path(&imported.path).unwrap().len(), 2);
+
+    let removed = dir.join("dicts/removed");
+    std::fs::create_dir_all(&removed).unwrap();
+    std::fs::rename(&imported.path, removed.join("law.qj")).unwrap();
+    poll(&mut router);
+    assert!(router.engine.extra_dictionaries().is_empty());
+    assert_eq!(std::fs::read(&config_path).unwrap(), original_config);
+    drop(router);
+    std::fs::remove_dir_all(&dir).unwrap();
+}
+
+#[test]
+fn dictionary_changes_do_not_retry_broken_config() {
+    let dir = std::env::temp_dir().join(format!("qingjian-reload-broken-{}", std::process::id()));
+    std::fs::create_dir_all(&dir).unwrap();
+    let config_path = dir.join("config.toml");
+    std::fs::write(&config_path, "[general]\npage_size = 5\n").unwrap();
+    modified_at(&config_path, 100);
+    let config = Config::load(&config_path).unwrap();
+    let mut router = Router::new(
+        Engine::new(Dictionary::default()),
+        RouterConfig::from(&config),
+    );
+    router.watch_config(&config, config_path.clone(), dir.clone(), Some(dir.clone()));
+
+    std::fs::write(&config_path, "[broken").unwrap();
+    modified_at(&config_path, 200);
+    poll(&mut router);
+    let source = dir.join("law.dict.yaml");
+    std::fs::write(&source, "---\nname: law\n...\n合同法\the tong fa\t120\n").unwrap();
+    let imported = import::import(&source, &dir.join("dicts")).unwrap();
+    poll(&mut router);
+    assert_eq!(router.config.page_size, 5);
+    assert_eq!(router.engine.extra_dictionaries().len(), 1);
+
+    // 保持坏配置的 mtime，以可观察的配置值确认后续轮询不会重新读它。
+    std::fs::write(&config_path, "[general]\npage_size = 9\n").unwrap();
+    modified_at(&config_path, 200);
+    poll(&mut router);
+    assert_eq!(router.config.page_size, 5);
+    let removed = dir.join("dicts/removed");
+    std::fs::create_dir_all(&removed).unwrap();
+    std::fs::rename(&imported.path, removed.join("law.qj")).unwrap();
+    poll(&mut router);
+    assert_eq!(router.config.page_size, 5);
+    assert!(router.engine.extra_dictionaries().is_empty());
+
+    modified_at(&config_path, 300);
+    poll(&mut router);
+    assert_eq!(router.config.page_size, 9);
+    drop(router);
+    std::fs::remove_dir_all(&dir).unwrap();
+}

rambocode/glimmer

设置页导入改走 glimmer-dictionary::import 转成 .qj（原来只复制 yaml，Server 不装），多选批量、自动启用、页面显示结果；Server 每次轮询比对 dicts\ 快照，新增 / 同名更新 / 移除不用重启；空词库拒绝导入。修 #36。
(cherry picked from commit 995cebf8deeb5e049ea294c0ff8b70624ec85b62)

apps/windows/settings/src/panel/pages/dictionaries.rs
@@ -150,12 +150,13 @@ pub(crate) fn view(settings: &Settings, context: &mut ViewContext<Settings>) -> 
                     .content("导入词库…"),
                 note("接受微明 TSV、Rime .dict.yaml、.qj；导入即复制进上面的目录。"),
             )),
+        note(&settings.dictionary_status),
     ]);
     page("词库", body)
 }
 
 /// 挪进 `dicts\removed`，不真删（与 macOS 一致）。
-pub(crate) fn remove_user_dict(settings: &Settings, stem: &str) {
+pub(crate) fn remove_user_dict(settings: &mut Settings, stem: &str) {
     let dir = user_dir(settings);
     let Some((_, path)) = extra_dictionaries::list(&dir)
         .into_iter()
@@ -165,35 +166,71 @@ pub(crate) fn remove_user_dict(settings: &Settings, stem: &str) {
     };
     let removed = dir.join("removed");
     if let Err(error) = std::fs::create_dir_all(&removed) {
-        crate::log::warn(format!("建 removed 目录失败: {error}"));
+        settings.dictionary_status = format!("移除失败：{error}");
         return;
     }
-    if let Some(file_name) = path.file_name()
-        && let Err(error) = std::fs::rename(&path, removed.join(file_name))
-    {
-        crate::log::warn(format!("移除词库 {stem} 失败: {error}"));
+    if let Some(file_name) = path.file_name() {
+        settings.dictionary_status = match std::fs::rename(&path, removed.join(file_name)) {
+            Ok(()) => format!("已移除「{stem}」，输入法将自动更新。"),
+            Err(error) => format!("移除失败：{error}"),
+        };
     }
 }
 
-/// 文件选择器选一本，复制进用户词库目录。
-pub(crate) fn import(settings: &Settings) {
-    let Some(source) = rfd::FileDialog::new()
+/// 多选词库，逐个转换并汇总结果；成功项的开关一次写回。
+pub(crate) fn import(settings: &mut Settings) {
+    let Some(sources) = rfd::FileDialog::new()
         .add_filter("词库文件", &["tsv", "yaml", "yml", "qj"])
         .add_filter("所有文件", &["*"])
         .set_title("导入词库")
-        .pick_file()
+        .pick_files()
     else {
         return;
     };
     let dir = user_dir(settings);
-    if let Err(error) = std::fs::create_dir_all(&dir) {
-        crate::log::warn(format!("建用户词库目录失败: {error}"));
-        return;
+    let mut disabled = settings.config.dictionaries.disabled.clone();
+    let mut results = Vec::new();
+    let mut succeeded = 0;
+    for source in &sources {
+        match glimmer_core::dictionary::import::import(source, &dir) {
+            Ok(imported) => {
+                let stem = imported
+                    .path
+                    .file_stem()
+                    .and_then(|s| s.to_str())
+                    .expect("dictionary importer produces a UTF-8 file stem");
+                disabled.retain(|name| name != stem);
+                succeeded += 1;
+                results.push(format!(
+                    "已导入「{}」，共 {} 条。",
+                    imported.name, imported.entries
+                ));
+            }
+            Err(error) => {
+                let message = format!("{} 导入失败：{error}", source.display());
+                crate::log::warn(&message);
+                results.push(message);
+            }
+        }
     }
-    let Some(file_name) = source.file_name() else {
-        return;
-    };
-    if let Err(error) = std::fs::copy(&source, dir.join(file_name)) {
-        crate::log::warn(format!("导入词库失败: {error}"));
+    let mut summary = format!(
+        "导入完成：成功 {} 个，失败 {} 个。",
+        succeeded,
+        sources.len() - succeeded
+    );
+    if disabled != settings.config.dictionaries.disabled
+        && let Err(error) = glimmer_platform::Config::set_array(
+            &settings.path,
+            "dictionaries",
+            "disabled",
+            &disabled,
+        )
+    {
+        results.push(format!(
+            "自动启用失败：{error}。此前关闭的词库需手动勾选启用。"
+        ));
+    } else if succeeded > 0 {
+        summary.push_str("输入法将自动加载。");
     }
+    settings.dictionary_status = format!("{summary}\n{}", results.join("\n"));
 }

crates/glimmer-server/src/dispatch/reload/state.rs
@@ -4,8 +4,11 @@ use std::path::PathBuf;
 use std::time::{Instant, SystemTime};
 
 use glimmer_core::Language;
-use glimmer_platform::DictionariesConfig;
+use glimmer_dictionary::Dictionary;
+use glimmer_platform::{DictionariesConfig, extra_dictionaries};
 use glimmer_predict::PredictConfig;
+
+use crate::assembly::user_dicts_dir;
 
 /// 热加载状态。
 pub(crate) struct ConfigReload {
@@ -38,4 +41,20 @@ pub(crate) struct ConfigReload {
 
     /// 已应用的学习语言（`None` 为关）。
     pub(super) applied_language: Option<Language>,
+
+    /// 最近加载的用户词库文件快照（路径、修改时间、长度）。
+    pub(super) dictionary_files: Vec<(PathBuf, Option<SystemTime>, u64)>,
+}
+
+impl ConfigReload {
+    /// 按上次有效配置装配词库，不把用户目录中的学习数据当作词库。
+    pub(super) fn load_dictionaries(&self) -> Vec<Dictionary> {
+        let dictionaries = extra_dictionaries::load(
+            self.bundled_dicts_dir.as_deref(),
+            user_dicts_dir(self.user_dir.as_deref()).as_deref(),
+            &self.applied_dictionaries,
+        );
+        tracing::info!(count = dictionaries.len(), "附加词库已热重装");
+        dictionaries
+    }
 }

crates/glimmer-server/src/dispatch/reload/tests.rs
@@ -1,0 +1,148 @@
+//! 词库文件变化在配置不变时仍应进入正在运行的引擎。
+
+use std::path::Path;
+use std::time::{Duration, Instant, SystemTime};
+
+use glimmer_core::Engine;
+use glimmer_dictionary::{Dictionary, import};
+use glimmer_platform::Config;
+
+use super::CONFIG_POLL_INTERVAL;
+use crate::dispatch::{Router, RouterConfig};
+
+fn poll(router: &mut Router) {
+    router.reload.as_mut().unwrap().last_check = Instant::now() - CONFIG_POLL_INTERVAL;
+    router.poll_config_reload();
+}
+
+fn modified_at(path: &Path, seconds: u64) {
+    std::fs::File::options()
+        .write(true)
+        .open(path)
+        .unwrap()
+        .set_modified(SystemTime::UNIX_EPOCH + Duration::from_secs(seconds))
+        .unwrap();
+}
+
+#[test]
+fn import_replace_and_remove_without_config_changes() {
+    let dir = std::env::temp_dir().join(format!("glimmer-reload-{}", std::process::id()));
+    std::fs::create_dir_all(&dir).unwrap();
+    let config_path = dir.join("config.toml");
+    std::fs::write(&config_path, "").unwrap();
+    let config = Config::load(&config_path).unwrap();
+    let original_config = std::fs::read(&config_path).unwrap();
+    let mut router = Router::new(Engine::new(Dictionary::default()), RouterConfig::default());
+    router.watch_config(
+        &config,
+        config_path.clone(),
+        dir.clone(),
+        Some(dir.clone()),
+        None,
+    );
+
+    let source = dir.join("law.dict.yaml");
+    std::fs::write(&source, "---\nname: law\n...\n合同法\the tong fa\t120\n").unwrap();
+    let imported = import::import(&source, &dir.join("dicts")).unwrap();
+    modified_at(&imported.path, 100);
+    poll(&mut router);
+    let dictionaries = router.engine.extra_dictionaries();
+    assert_eq!(dictionaries.len(), 1);
+    assert_eq!(
+        dictionaries[0].lookup(&["he", "tong", "fa"], false)[0].text,
+        "合同法"
+    );
+
+    // 只改词频，输出长度保持不变，必须靠 mtime 发现更新。
+    let original_len = std::fs::metadata(&imported.path).unwrap().len();
+    std::fs::write(&source, "---\nname: law\n...\n合同法\the tong fa\t121\n").unwrap();
+    import::import(&source, &dir.join("dicts")).unwrap();
+    modified_at(&imported.path, 200);
+    assert_eq!(
+        std::fs::metadata(&imported.path).unwrap().len(),
+        original_len
+    );
+    poll(&mut router);
+    assert_eq!(
+        router.engine.extra_dictionaries()[0].lookup(&["he", "tong", "fa"], false)[0].frequency,
+        121
+    );
+
+    // 保持旧词库映射打开，模拟运行中的 Server 收到同名替换。
+    std::fs::write(
+        &source,
+        "---\nname: law\n...\n民法典\tmin fa dian\t100\n法律\tfa lv\t30\n",
+    )
+    .unwrap();
+    import::import(&source, &dir.join("dicts")).unwrap();
+    poll(&mut router);
+    let dictionary = &router.engine.extra_dictionaries()[0];
+    assert!(dictionary.lookup(&["he", "tong", "fa"], false).is_empty());
+    assert_eq!(
+        dictionary.lookup(&["min", "fa", "dian"], false)[0].text,
+        "民法典"
+    );
+
+    // YAML 头中只有引用、没有本文件词条时，失败不能覆盖已有词库。
+    std::fs::write(&source, "---\nname: law\nimport_tables:\n  - other\n...\n").unwrap();
+    assert!(import::import(&source, &dir.join("dicts")).is_err());
+    assert_eq!(Dictionary::from_path(&imported.path).unwrap().len(), 2);
+
+    let removed = dir.join("dicts/removed");
+    std::fs::create_dir_all(&removed).unwrap();
+    std::fs::rename(&imported.path, removed.join("law.qj")).unwrap();
+    poll(&mut router);
+    assert!(router.engine.extra_dictionaries().is_empty());
+    assert_eq!(std::fs::read(&config_path).unwrap(), original_config);
+    drop(router);
+    std::fs::remove_dir_all(&dir).unwrap();
+}
+
+#[test]
+fn dictionary_changes_do_not_retry_broken_config() {
+    let dir = std::env::temp_dir().join(format!("glimmer-reload-broken-{}", std::process::id()));
+    std::fs::create_dir_all(&dir).unwrap();
+    let config_path = dir.join("config.toml");
+    std::fs::write(&config_path, "[general]\npage_size = 5\n").unwrap();
+    modified_at(&config_path, 100);
+    let config = Config::load(&config_path).unwrap();
+    let mut router = Router::new(
+        Engine::new(Dictionary::default()),
+        RouterConfig::from(&config),
+    );
+    router.watch_config(
+        &config,
+        config_path.clone(),
+        dir.clone(),
+        Some(dir.clone()),
+        None,
+    );
+
+    std::fs::write(&config_path, "[broken").unwrap();
+    modified_at(&config_path, 200);
+    poll(&mut router);
+    let source = dir.join("law.dict.yaml");
+    std::fs::write(&source, "---\nname: law\n...\n合同法\the tong fa\t120\n").unwrap();
+    let imported = import::import(&source, &dir.join("dicts")).unwrap();
+    poll(&mut router);
+    assert_eq!(router.config.page_size, 5);
+    assert_eq!(router.engine.extra_dictionaries().len(), 1);
+
+    // 保持坏配置的 mtime，以可观察的配置值确认后续轮询不会重新读它。
+    std::fs::write(&config_path, "[general]\npage_size = 9\n").unwrap();
+    modified_at(&config_path, 200);
+    poll(&mut router);
+    assert_eq!(router.config.page_size, 5);
+    let removed = dir.join("dicts/removed");
+    std::fs::create_dir_all(&removed).unwrap();
+    std::fs::rename(&imported.path, removed.join("law.qj")).unwrap();
+    poll(&mut router);
+    assert_eq!(router.config.page_size, 5);
+    assert!(router.engine.extra_dictionaries().is_empty());
+
+    modified_at(&config_path, 300);
+    poll(&mut router);
+    assert_eq!(router.config.page_size, 9);
+    drop(router);
+    std::fs::remove_dir_all(&dir).unwrap();
+}